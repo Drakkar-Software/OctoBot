662c7389f98e7d418bfa3322b98b77f73c875b4f
Paul Bouquet <paul.bouquet@gmail.com>
2018-03-29 01:12:01 +0200

    Add indicator calculation

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from binance.client import Client
+from botcore.config.config import load_config
+
+
 def main():
-    pass
+    config = load_config("config.json")
+    client = Client(config["exchanges"]["binance"]["api-key"],
+                    config["exchanges"]["binance"]["api-secret"],
+                    {"verify": True, "timeout": 20})
+    print(client.get_historical_trades(symbol='BTCUSDT'))
 
 
 if __name__ == '__main__':
