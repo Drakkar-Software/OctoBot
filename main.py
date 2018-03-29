6b403e2229699c8c0fbe06c73dc891872f41ab47
Paul Bouquet <paul.bouquet@gmail.com>
2018-03-29 23:17:40 +0200

    Prepare TA evaluator classes

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from botcore.config.config import load_config
 
 from indicator import *
 
+STR_PRICE_CLOSE = "<CLOSE>"
+STR_PRICE_OPEN = "<OPEN>"
+STR_PRICE_HIGH = "<HIGH>"
+STR_PRICE_LOW = "<LOW>"
+STR_PRICE_VOL = "<VOL>"
 
-# https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md
 
 def main():
     config = load_config("config.json")
@@ -12,15 +16,19 @@ def main():
                     config["exchanges"]["binance"]["api-secret"],
                     {"verify": True, "timeout": 20})
 
-    candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_1HOUR)
-    prices = {"<CLOSE>": [], "<VOL>": []}
+    candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_1DAY)
+    prices = {STR_PRICE_HIGH: [], STR_PRICE_LOW: [], STR_PRICE_OPEN: [], STR_PRICE_CLOSE: [], STR_PRICE_VOL: []}
 
     for c in candles:
-        prices["<CLOSE>"].append(float(c[4]))
-        prices["<VOL>"].append(float(c[5]))
+        prices[STR_PRICE_OPEN].append(float(c[1]))
+        prices[STR_PRICE_HIGH].append(float(c[2]))
+        prices[STR_PRICE_LOW].append(float(c[3]))
+        prices[STR_PRICE_CLOSE].append(float(c[4]))
+        prices[STR_PRICE_VOL].append(float(c[5]))
 
     df = pd.DataFrame(data=prices)
-    print(on_balance_volume(df))
+    print(williams_r(df))
+
 
 
 if __name__ == '__main__':
