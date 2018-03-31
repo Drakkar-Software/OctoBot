c245ef6b0de7aab66cffc894018b1f1c3a5055af
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-01 01:12:28 +0200

    #2 [Exchange][Global] Define parent exchange class architecture
    #2 [Exchange][Global] Define parent exchange class architecture

--- exchanges/exchange.py
+++ exchanges/exchange.py
@@ -1,14 +1,27 @@
-from config.cst import *
+from abc import *
 
 
 class Exchange:
+    __metaclass__ = ABCMeta
+
     def __init__(self, config):
         self.config = config
+        self.symbol_list = []
 
     # @return DataFrame of prices
+    @abstractmethod
     def get_symbol_prices(self, symbol, time_frame):
-        pass
+        raise NotImplementedError("Get_symbol_prices not implemented")
+
+    @abstractmethod
+    def get_symbol_list(self):
+        raise NotImplementedError("Get_symbol_list not implemented")
+
+    @abstractmethod
+    def symbol_exists(self, symbol):
+        raise NotImplementedError("Symbol_exists not implemented")
 
     @staticmethod
+    @abstractmethod
     def get_time_frame_enum():
-        return TimeFrames
+        raise NotImplementedError("Get_time_frame_enum not implemented")
