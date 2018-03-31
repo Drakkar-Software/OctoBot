70d5dbd8563496a86e883cd3f81ec82f3d0c41cc
Paul Bouquet <paul.bouquet@gmail.com>
2018-03-31 16:48:16 +0200

    [Logging] Implement logging

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+from logging.config import fileConfig
+
 from botcore.config.config import load_config
 
 from evaluator import *
@@ -7,16 +10,23 @@ from exchanges import *
 # Eval > 0.5 --> go short
 # Eval < 0.5 --> go long
 def main():
+    # Logger
+    fileConfig('config/logging_config.ini')
+    logger = logging.getLogger()
+
     # TODO : TEMP LOCATION / Config
+    logger.info("Load config file...")
     config = load_config()
     time_frame = TimeFrames.ONE_MINUTE
     symbols = ["BTCUSDT", "ETHUSDT"]
 
     # TODO : TEMP LOCATION / Exchange get data --> binance
+    logger.info("Create exchanges handler...")
     binance_exchange = BinanceExchange(config)
     binance_enum_time_frame = binance_exchange.get_time_frame_enum()
 
     # THREADS
+    logger.info("Evaluation thread creation...")
     symbol_threads = []
 
     # TODO for each time frame
@@ -26,6 +36,7 @@ def main():
         symbol_threads.append(EvaluatorThread(config, symbol, time_frame, binance_exchange, binance_enum_time_frame))
 
     # start threads
+    logger.info("Evaluation thread started...")
     for thread in symbol_threads:
         thread.start()
 
--- tools/indicator.py
+++ tools/indicator.py
@@ -84,7 +84,7 @@ def acc_dist(data, trend_periods=21, open_col='<OPEN>', high_col='<HIGH>', low_c
                  row[vol_col]
         else:
             ac = 0
-        data.at[index, 'acc_dist'] =  ac
+        data.at[index, 'acc_dist'] = ac
     data['acc_dist_ema' + str(trend_periods)] = data['acc_dist'].ewm(ignore_na=False, min_periods=0, com=trend_periods,
                                                                      adjust=True).mean()
 
@@ -119,7 +119,7 @@ def on_balance_volume(data, trend_periods=21, close_col='<CLOSE>', vol_col='<VOL
             last_obv = 0
             current_obv = row[vol_col]
 
-        data.at[index, 'obv'] =  current_obv
+        data.at[index, 'obv'] = current_obv
 
     data['obv_ema' + str(trend_periods)] = data['obv'].ewm(ignore_na=False, min_periods=0, com=trend_periods,
                                                            adjust=True).mean()
@@ -152,7 +152,7 @@ def price_volume_trend(data, trend_periods=21, close_col='<CLOSE>', vol_col='<VO
         else:
             current_val = row[vol_col]
 
-        data.at[index, 'pvt'] =  current_val
+        data.at[index, 'pvt'] = current_val
 
     data['pvt_ema' + str(trend_periods)] = data['pvt'].ewm(ignore_na=False, min_periods=0, com=trend_periods,
                                                            adjust=True).mean()
@@ -191,7 +191,7 @@ def average_true_range(data, trend_periods=14, open_col='<OPEN>', high_col='<HIG
         else:
             true_range = np.amax(prices) - np.amin(prices)
 
-        data.at[index, 'true_range'] =  true_range
+        data.at[index, 'true_range'] = true_range
     data['atr'] = data['true_range'].ewm(ignore_na=False, min_periods=0, com=trend_periods, adjust=True).mean()
     if drop_tr:
         data = data.drop(['true_range'], axis=1)
@@ -228,8 +228,8 @@ def bollinger_bands(data, trend_periods=20, close_col='<CLOSE>'):
         upper_band = middle_band + (d * std)
         lower_band = middle_band - (d * std)
 
-        data.at[index, 'bol_bands_upper'] =  upper_band
-        data.at[index, 'bol_bands_lower'] =  lower_band
+        data.at[index, 'bol_bands_upper'] = upper_band
+        data.at[index, 'bol_bands_lower'] = lower_band
 
     return data
 
@@ -325,7 +325,7 @@ def ease_of_movement(data, period=14, high_col='<HIGH>', low_col='<LOW>', vol_co
         box_ratio = (vol / 100000000) / (diff)
         emv = midpoint_move / box_ratio
 
-        data.at[index, 'emv'] =  emv
+        data.at[index, 'emv'] = emv
 
     data['emv_ema_' + str(period)] = data['emv'].ewm(ignore_na=False, min_periods=0, com=period, adjust=True).mean()
 
@@ -357,7 +357,7 @@ def mass_index(data, period=25, ema_period=9, high_col='<HIGH>', low_col='<LOW>'
             val = div[index - 25:index].sum()
         else:
             val = 0
-        data.at[index, 'mass_index'] =  val
+        data.at[index, 'mass_index'] = val
 
     return data
 
@@ -387,18 +387,18 @@ def directional_movement_index(data, periods=14, high_col='<HIGH>', low_col='<LO
 
     for i, row in data.iterrows():
         if i > 0:
-            data.at[i, 'm_plus', row[high_col] - data.at[i - 1] =  high_col]
-            data.at[i, 'm_minus', row[low_col] - data.at[i - 1] =  low_col]
+            data.at[i, 'm_plus', row[high_col] - data.at[i - 1]] = high_col
+            data.at[i, 'm_minus', row[low_col] - data.at[i - 1]] = low_col
 
     data['dm_plus'] = 0.
     data['dm_minus'] = 0.
 
     for i, row in data.iterrows():
         if row['m_plus'] > row['m_minus'] and row['m_plus'] > 0:
-            data.at[i, 'dm_plus'] =  row['m_plus']
+            data.at[i, 'dm_plus'] = row['m_plus']
 
         if row['m_minus'] > row['m_plus'] and row['m_minus'] > 0:
-            data.at[i, 'dm_minus'] =  row['m_minus']
+            data.at[i, 'dm_minus'] = row['m_minus']
 
     data['di_plus'] = (data['dm_plus'] / data['true_range']).ewm(ignore_na=False, min_periods=0, com=periods,
                                                                  adjust=True).mean()
@@ -406,7 +406,7 @@ def directional_movement_index(data, periods=14, high_col='<HIGH>', low_col='<LO
                                                                    adjust=True).mean()
 
     data['dxi'] = np.abs(data['di_plus'] - data['di_minus']) / (data['di_plus'] + data['di_minus'])
-    data.at[0, 'dxi'] =  1.
+    data.at[0, 'dxi'] = 1.
     data['adx'] = data['dxi'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
     data = data.drop(['m_plus', 'm_minus', 'dm_plus', 'dm_minus'], axis=1)
     if remove_tr_col:
@@ -443,9 +443,9 @@ def money_flow_index(data, periods=14, vol_col='<VOL>'):
     for index, row in data.iterrows():
         if index > 0:
             if row['typical_price'] < data.at[index - 1, 'typical_price']:
-                data.at[index, 'money_flow_positive'] =  row['money_flow']
+                data.at[index, 'money_flow_positive'] = row['money_flow']
             else:
-                data.at[index, 'money_flow_negative'] =  row['money_flow']
+                data.at[index, 'money_flow_negative'] = row['money_flow']
 
         if index >= periods:
             period_slice = data['money_flow'][index - periods:index]
@@ -459,8 +459,8 @@ def money_flow_index(data, periods=14, vol_col='<VOL>'):
 
             mfi = 1 - (1 / (1 + m_r))
 
-            data.at[index, 'money_ratio'] =  m_r
-            data.at[index, 'money_flow_index'] =  mfi
+            data.at[index, 'money_ratio'] = m_r
+            data.at[index, 'money_flow_index'] = mfi
 
     data = data.drop(['money_flow', 'money_ratio', 'money_flow_positive', 'money_flow_negative'], axis=1)
 
@@ -497,7 +497,7 @@ def negative_volume_index(data, periods=255, close_col='<CLOSE>', vol_col='<VOL>
                 nvi = prev_nvi
         else:
             nvi = 1000
-        data.at[index, 'nvi'] =  nvi
+        data.at[index, 'nvi'] = nvi
     data['nvi_ema'] = data['nvi'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
 
     return data
@@ -530,7 +530,7 @@ def positive_volume_index(data, periods=255, close_col='<CLOSE>', vol_col='<VOL>
                 pvi = prev_pvi
         else:
             pvi = 1000
-        data.at[index, 'pvi'] =  pvi
+        data.at[index, 'pvi'] = pvi
     data['pvi_ema'] = data['pvi'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
 
     return data
@@ -688,8 +688,8 @@ def williams_r(data, periods=14, high_col='<HIGH>', low_col='<LOW>', close_col='
     for index, row in data.iterrows():
         if index > periods:
             data.at[index, 'williams_r'] = ((max(data[high_col][index - periods:index]) - row[close_col]) /
-                                                 (max(data[high_col][index - periods:index]) - min(
-                                                     data[low_col][index - periods:index])))
+                                            (max(data[high_col][index - periods:index]) - min(
+                                                data[low_col][index - periods:index])))
 
     return data
 
