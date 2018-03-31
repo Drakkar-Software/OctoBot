a47e53cfde57c9ed49ae0dd25713a56f8a86697c
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-01 01:12:28 +0200

    #2 [Exchange][Global] Define parent exchange class architecture
    #2 [Exchange][Global] Define parent exchange class architecture

--- main.py
+++ main.py
@@ -13,28 +13,36 @@ def main():
     fileConfig('config/logging_config.ini')
     logger = logging.getLogger()
 
-    # TODO : TEMP LOCATION
+    # Config
     logger.info("Load config file...")
     config = load_config()
+
+    # TODO : CONFIG TEMP LOCATION
     time_frames = [TimeFrames.ONE_MINUTE]
     symbols = ["BTCUSDT", "ETHUSDT"]
     exchanges = [BinanceExchange]
 
     # THREADS
-    logger.info("Evaluation thread creation...")
+    logger.info("Evaluation threads creation...")
     symbols_threads = []
 
     # create symbol threads
     for symbol in symbols:
+
         for exchange_type in exchanges:
             exchange_inst = exchange_type(config)
-            for time_frame in time_frames:
-                symbols_threads.append(EvaluatorThread(config, symbol, time_frame, exchange_inst))
+            exchange_inst.get_symbol_list()
+
+            # Verify that symbol exists on this exchange
+            if exchange_inst.symbol_exists(symbol):
+
+                for time_frame in time_frames:
+                    symbols_threads.append(EvaluatorThread(config, symbol, time_frame, exchange_inst))
 
     # start threads
-    logger.info("Evaluation thread started...")
     for thread in symbols_threads:
         thread.start()
+    logger.info("Evaluation threads started...")
 
     # join threads
     for thread in symbols_threads:
