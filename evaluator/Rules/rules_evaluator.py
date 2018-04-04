346eb66e86b8fa39ee51c823b2488942e3971cf6
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-04 22:09:28 +0200

    #30 Add rules arch

---
+++ evaluator/Rules/rules_evaluator.py
@@ -0,0 +1,38 @@
+from abc import *
+
+from config.cst import *
+
+
+class RulesEvaluator:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        super().__init__()
+        self.logger = None
+        self.config = None
+        self.eval_note = START_EVAL_NOTE
+        self.pertinence = START_EVAL_PERTINENCE
+        self.logger = None
+        self.enabled = True
+
+    def set_logger(self, logger):
+        self.logger = logger
+
+    def set_config(self, config):
+        self.config = config
+
+    def get_eval_note(self):
+        return self.eval_note
+
+    def get_pertinence(self):
+        return self.pertinence
+
+    def get_is_enabled(self):
+        return self.enabled
+
+    def get_evaluator_name(self):
+        return self.__class__.__name__
+
+    @abstractmethod
+    def eval(self):
+        raise NotImplementedError("Eval not implemented")
