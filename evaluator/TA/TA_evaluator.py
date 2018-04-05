48979d4609475ccfb413e8c5a3340fc36fa63369
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-05 22:09:21 +0200

    #33 setup matrix

--- evaluator/TA/TA_evaluator.py
+++ evaluator/TA/TA_evaluator.py
@@ -45,9 +45,6 @@ class TAEvaluator:
     def get_is_enabled(self):
         return self.enabled
 
-    def get_evaluator_name(self):
-        return self.__class__.__name__
-
     @abstractmethod
     def eval(self):
         raise NotImplementedError("Eval not implemented")
---
+++ evaluator/evaluator_matrix.py
@@ -0,0 +1,20 @@
+from config.cst import EvaluatorMatrixTypes
+
+
+class EvaluatorMatrix:
+    def __init__(self):
+        self.matrix = {
+            EvaluatorMatrixTypes.TA: {},
+            EvaluatorMatrixTypes.SOCIAL: {},
+            EvaluatorMatrixTypes.REAL_TIME: {}
+        }
+
+    # ---- getters and setters----
+    def set_eval(self, matrix_type, evaluator_name, value):
+        self.matrix[matrix_type][evaluator_name] = value
+
+    def get_type_evals(self, matrix_type):
+        return self.matrix[matrix_type]
+
+    def get_matrix(self):
+        return self.matrix
