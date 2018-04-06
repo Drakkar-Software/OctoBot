6b388d8043938df6ca9aa02df4c0007040a5ceb4
GuillaumeDSM <g.dsm@laposte.net>
2018-04-06 20:11:11 +0200

    #41 fixed 2 minutes test

--- tests/test_start_bot.py
+++ tests/test_start_bot.py
@@ -13,13 +13,14 @@ def test_create_bot():
 def test_run_bot():
     # launch a bot
     bot = Crypto_Bot()
-    bot.set_time_frames[TimeFrames.ONE_MINUTE]
+    bot.set_time_frames([TimeFrames.ONE_MINUTE])
     bot.create_exchange_traders()
     bot.create_evaluation_threads()
     bot.start_threads()
 
     # let it run 2 minutes: test will fail if an exception is raised
-    time.sleep(2)
+    # 1.9 to stop threads before the next time frame
+    time.sleep(1.9*60)
 
     # stop the bot
     bot.stop_threads()
