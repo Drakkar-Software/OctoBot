3d9c75a7817a22fd1d6bb450538227180a21b35b
GuillaumeDSM <g.dsm@laposte.net>
2018-04-06 19:06:53 +0200

    #42 added 2 minutes run test

--- tests/test_start_bot.py
+++ tests/test_start_bot.py
@@ -1,5 +1,18 @@
+import time
+
 from bot import Crypto_Bot
 
 
 def test_create_bot():
+    # launch a bot
     bot = Crypto_Bot()
+
+
+def test_run_bot():
+    bot = Crypto_Bot()
+    bot.create_exchange_traders()
+    bot.create_evaluation_threads()
+    bot.start_threads()
+
+    # let it run 2 minutes: test will fail if an exception is raised
+    time.sleep(2*60)
