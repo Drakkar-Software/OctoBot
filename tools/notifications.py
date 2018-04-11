74ec50d510d7fa826b4462da5d04b13e9f40f511
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-11 20:15:37 +0200

    Implement twitter notification

--- tools/notifications.py
+++ tools/notifications.py
@@ -2,6 +2,7 @@ import logging
 from enum import Enum
 
 from botcore.services.mail.send_gmail import GmailMailSendFactory
+from botcore.social.twitter.post import TwitterPostFactory
 
 
 class Notification:
@@ -23,11 +24,11 @@ class Notification:
             return False
 
     def notify(self, symbol, result, matrix):
-        if self.notification_type == NotificationTypes.MAIL.value:
+        if NotificationTypes.MAIL.value in self.notification_type:
             # if config contains enough data for mailing
             if self.mail_enabled():
                 mail = GmailMailSendFactory(self.config)
-                mail.set_to(self.config["service"]["mail"]["mail_dest"])
+                mail.set_to(self.config["services"]["mail"]["mail_dest"])
                 mail.set_subject("CRYPTO BOT ALERT : " + symbol + " / " + str(result))
                 mail.set_content("CRYPTO BOT ALERT : " + symbol + " / " + str(result) + "\n MATRIX : " + str(matrix))
                 mail.send()
@@ -35,14 +36,30 @@ class Notification:
             else:
                 self.logger.debug("Mail disabled")
 
-    def mail_enabled(self):
-        if self.service_enabled() and "mail" in self.config["service"]:
+        if NotificationTypes.TWITTER.value in self.notification_type:
+            # if config contains enough data for twitting
+            if self.twitter_enabled():
+                twitter = TwitterPostFactory(self.config)
+                twitter.set_content("CRYPTO BOT ALERT : " + symbol + " / " + str(result))
+                twitter.upload_post()
+                self.logger.info("Twitter sent")
+            else:
+                self.logger.debug("Twitter notification disabled")
+
+    def twitter_enabled(self):
+        if self.services_enabled() and "twitter" in self.config["services"]:
             return True
         else:
             return False
 
-    def service_enabled(self):
-        if "service" in self.config:
+    def mail_enabled(self):
+        if self.services_enabled() and "mail" in self.config["services"]:
+            return True
+        else:
+            return False
+
+    def services_enabled(self):
+        if "services" in self.config:
             return True
         else:
             return False
@@ -50,3 +67,4 @@ class Notification:
 
 class NotificationTypes(Enum):
     MAIL = 1
+    TWITTER = 2
