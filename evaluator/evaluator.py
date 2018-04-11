40567a2b932a6eb6036a715a2f31a5f1c4a02c8d
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-11 19:29:50 +0200

    #55 [EvaluatorCreator] move evaluator creator's setters/getters to evaluator

--- evaluator/evaluator.py
+++ evaluator/evaluator.py
@@ -13,7 +13,11 @@ class Evaluator:
         self.symbol_evaluator = None
         self.data_changed = False
 
-        self.creator = EvaluatorCreator(self)
+        self.social_eval_list = []
+        self.ta_eval_list = []
+        self.real_time_eval_list = []
+
+        self.creator = EvaluatorCreator()
 
     def set_config(self, config):
         self.config = config
@@ -38,16 +42,35 @@ class Evaluator:
     def set_symbol_evaluator(self, symbol_evaluator):
         self.symbol_evaluator = symbol_evaluator
 
+    def set_social_eval(self, new_social_list, evaluator_thread):
+        self.social_eval_list = new_social_list
+        for social_eval in self.social_eval_list:
+            social_eval.add_evaluator_thread(evaluator_thread)
+
+    def set_real_time_eval(self, new_real_time_list, evaluator_thread):
+        self.real_time_eval_list = new_real_time_list
+        for real_time_eval in self.real_time_eval_list:
+            real_time_eval.add_evaluator_thread(evaluator_thread)
+
     def update_ta_eval(self, ignored_evaluator=None):
         # update only with new data
         if self.data_changed:
-            for ta_evaluator in self.creator.get_ta_eval_list():
+            for ta_evaluator in self.get_ta_eval_list():
                 ta_evaluator.set_data(self.data)
                 if not ta_evaluator.get_name() == ignored_evaluator and ta_evaluator.get_is_evaluable():
                     ta_evaluator.eval()
 
             # reset data changed after update
             self.data_changed = False
+
+    def get_social_eval_list(self):
+        return self.social_eval_list
+
+    def get_real_time_eval_list(self):
+        return self.real_time_eval_list
+
+    def get_ta_eval_list(self):
+        return self.ta_eval_list
 
     def get_data(self):
         return self.data
