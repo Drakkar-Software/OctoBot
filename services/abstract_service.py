2b0ea3e83fc4bad1f022d022582e6b4b626d4c9c
GuillaumeDSM <g.dsm@laposte.net>
2018-04-14 14:59:10 +0200

    #24#25 added services architecture and twitter_serice.py

---
+++ services/abstract_service.py
@@ -0,0 +1,42 @@
+from abc import *
+
+
+class AbstractService:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        super().__init__()
+        self.logger = None
+        self.config = None
+        self.enabled = True
+
+    @classmethod
+    def get_name(cls):
+        return cls.__name__
+
+    # Used to provide a new logger for this particular indicator
+    def set_logger(self, logger):
+        self.logger = logger
+
+    # Used to provide the global config
+    def set_config(self, config):
+        self.config = config
+
+    # If this indicator is enabled
+    def get_is_enabled(self):
+        return self.enabled
+
+    # Returns the service's endpoint
+    @abstractmethod
+    def get_endpoint(self) -> None:
+        raise NotImplementedError("get_endpoint not implemented")
+
+    # Called to put in the right service in config
+    @abstractmethod
+    def get_type(self) -> None:
+        raise NotImplementedError("get_type not implemented")
+
+    # Called after service setup
+    @abstractmethod
+    def prepare(self) -> None:
+        raise NotImplementedError("prepare not implemented")
