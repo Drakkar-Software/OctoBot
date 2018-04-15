d71197ef2973492002afbc2771abff0e7eb2752b
GuillaumeDSM <g.dsm@laposte.net>
2018-04-15 23:09:56 +0200

    #24#25 fixed encoding issues

--- tools/decoding_encoding.py
+++ tools/decoding_encoding.py
@@ -5,4 +5,8 @@ class DecoderEncoder:
 
     @staticmethod
     def decode_text(text):
-        return str(unicodedata.normalize('NFKD', text).encode("utf-8", 'ignore'))
+        return unicodedata.normalize('NFKD', text)
+
+    @staticmethod
+    def encode_into_bytes(text, encoding="utf-8", errors="ignore"):
+        return str(text.encode(encoding, errors))
