9d4b926490dd92e724fbce24740ed9a41a8a300a
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-21 02:20:10 +0200

    Fix raspberry installer

--- docs/install/raspberry-pre-install.sh
+++ docs/install/raspberry-pre-install.sh
@@ -1,11 +1,11 @@
-sudo apt-get install build-essential libc6-dev
-sudo apt-get install libncurses5-dev libncursesw5-dev libreadline6-dev
-sudo apt-get install libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
-sudo apt-get install libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+sudo apt-get install -y build-essential libc6-dev
+sudo apt-get install -y libncurses5-dev libncursesw5-dev libreadline6-dev
+sudo apt-get install -y libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
+sudo apt-get install -y libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 
-sudo apt install libxml2-dev libxslt1-dev
-sudo apt-get install libffi-dev
+sudo apt install -y libxml2-dev libxslt1-dev
+sudo apt install -y libffi-dev
 
 sudo add-apt-repository ppa:jonathonf/python-3.6
 sudo apt update
-sudo apt install python3.6 python3-pip
+sudo apt install -y python3.6 python3-pip
