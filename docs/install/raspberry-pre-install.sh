007a5fd0548a320b018a510f3ba8ec8f0470e9b6
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-21 02:28:43 +0200

    Prepare merge production version 0.0.7-alpha

--- docs/install/raspberry-pre-install.sh
+++ docs/install/raspberry-pre-install.sh
@@ -8,4 +8,4 @@ sudo apt install -y libffi-dev
 
 sudo add-apt-repository ppa:jonathonf/python-3.6
 sudo apt update
-sudo apt install -y python3.6 python3-pip
+sudo apt install -y python3 python3-pip
