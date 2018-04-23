2fa78e933a42fc58e549dad6bd61137bd9122207
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-24 00:24:07 +0200

    Prepare temporary production version

--- evaluator/Social/stats_evaluator.py
+++ evaluator/Social/stats_evaluator.py
@@ -13,7 +13,7 @@ class GoogleTrendStatsEvaluator(StatsSocialEvaluator):
     def __init__(self):
         super().__init__()
         self.pytrends = None
-        self.enabled = True
+        self.enabled = False
         self.is_threaded = False
 
     # Use pytrends lib (https://github.com/GeneralMills/pytrends)
