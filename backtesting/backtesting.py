f3035380824d604bf493625485ebc4c6ffd8e426
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-30 00:11:29 +0200

    Disable notification when backtesting

--- backtesting/backtesting.py
+++ backtesting/backtesting.py
@@ -1,13 +1,16 @@
+import time
+
 from config.cst import *
 
 
 class Backtesting:
     def __init__(self, config):
         self.config = config
+        self.begin_time = time.time()
 
     def end(self):
         # TODO : temp
-        raise Exception("End of simulation")
+        raise Exception("End of simulation in {0} sec".format(time.time() - self.begin_time))
 
     @staticmethod
     def enabled(config):
