30a1d34f38fadd22d1c1bbb21a6e15408e8c8fde
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-29 18:29:28 +0200

    fix travis

--- evaluator/abstract_evaluator.py
+++ evaluator/abstract_evaluator.py
@@ -116,7 +116,7 @@ class AbstractEvaluator:
             self.eval_note += new_eval_note
 
     def is_enabled(self, default):
-        if self.get_name() in self.config[CONFIG_EVALUATOR]:
+        if CONFIG_EVALUATOR in self.config and self.get_name() in self.config[CONFIG_EVALUATOR]:
             return self.config[CONFIG_EVALUATOR][self.get_name()]
         else:
             for parent in self.__class__.mro():
