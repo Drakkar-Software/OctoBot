98f293feb7a5225d69bf756f26c4951a01ad4be9
Paul Bouquet <paul.bouquet@gmail.com>
2018-04-29 18:57:52 +0200

    [Portfolio] Create test

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -6,16 +6,20 @@ from trading.trader.portfolio import Portfolio
 from trading.trader.trader_simulator import TraderSimulator
 
 
-class TestPortfolio(object):
-    def init_default(self):
-        config = load_test_config
+class TestPortfolio:
+    @staticmethod
+    def init_default():
+        config = load_test_config()
         exchange_inst = ExchangeSimulator(config, ccxt.binance)
         trader_inst = TraderSimulator(config, exchange_inst)
         portfolio_inst = Portfolio(config, trader_inst)
         return config, exchange_inst, trader_inst, portfolio_inst
 
-    def _load_portfolio_without_config(self):
-        config = None
-        test_inst = Portfolio(config, None)
+    def test_load_portfolio(self):
+        config, exchange_inst, trader_inst, portfolio_inst = self.init_default()
+
+        test_inst = Portfolio(config, trader_inst)
         test_inst._load_portfolio()
-        assert test_inst.portfolio is {}
+        assert test_inst.portfolio == {'BTC': {'available': 10, 'total': 10},
+                                       'USDT': {'available': 1000, 'total': 1000}
+                                       }
