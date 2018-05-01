87fedd9e6f4b68f32bdea5748daa311be26c3c09
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-01 19:15:51 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+from config.cst import VERSION
+
+DESCRIPTION = open('README.md').read() + '\n\n' + open('docs/CHANGELOG.md').read()
+
+setup(
+    name='coveralls',
+    version=VERSION,
+    packages=['coveralls'],
+    url='http://github.com/coveralls-clients/coveralls-python',
+    license='MIT',
+    author='coveralls-clients contributors',
+    description='Show coverage stats online via coveralls.io',
+    long_description=DESCRIPTION,
+    entry_points={
+        'console_scripts': [
+            'coveralls = coveralls.cli:main',
+        ],
+    },
+    # TODO install_requires=[],
+    setup_requires=['pytest-runner'],
+    # TODO  tests_require=[],
+    # TODO  extras_require={},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
