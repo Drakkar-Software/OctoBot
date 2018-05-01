02459859eb916620fbc182534d583392806d5fac
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-05-01 13:33:36 +0200

    #119 started TA test architecture

---
+++ tests/TA_evaluators_tests/test_macd_TA_evaluator.py
@@ -0,0 +1,9 @@
+from tests.TA_evaluators_tests.abstract_TA_test import AbstractTATest
+from evaluator.TA.momentum_evaluator import MACDMomentumEvaluator
+
+
+class TestMACDTAEvaluator(AbstractTATest):
+
+    def test_stress_test(self):
+        self.init(MACDMomentumEvaluator)
+        self.run_stress_test_without_exceptions()
