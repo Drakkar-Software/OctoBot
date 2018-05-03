1841ce77e88d6ff545e16325b85c73c65d7f8f63
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-04 00:48:35 +0200

    Implement Matrix history to provides data to interface

--- evaluator/evaluator_matrix.py
+++ evaluator/evaluator_matrix.py
@@ -1,8 +1,9 @@
-from config.cst import EvaluatorMatrixTypes
+from config.cst import EvaluatorMatrixTypes, CONFIG_DEBUG_MATRIX_HISTORY, CONFIG_DEBUG_MATRIX_HISTORY_FILE
 
 
 class EvaluatorMatrix:
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
         self.matrix = {
             EvaluatorMatrixTypes.TA: {},
             EvaluatorMatrixTypes.SOCIAL: {},
@@ -20,8 +21,25 @@ class EvaluatorMatrix:
         else:
             self.matrix[matrix_type][evaluator_name] = value
 
+        if EvaluatorMatrixHistory.enabled(self.config):
+            EvaluatorMatrixHistory.update_history(self.matrix)
+
     def get_type_evals(self, matrix_type):
         return self.matrix[matrix_type]
 
     def get_matrix(self):
         return self.matrix
+
+
+class EvaluatorMatrixHistory:
+    @staticmethod
+    def update_history(matrix):
+        with open(CONFIG_DEBUG_MATRIX_HISTORY_FILE, "a") as file:
+            file.write(matrix)
+
+    @staticmethod
+    def enabled(config):
+        if CONFIG_DEBUG_MATRIX_HISTORY in config and config[CONFIG_DEBUG_MATRIX_HISTORY]:
+            return True
+        else:
+            return False
