6793ca3e02aa72aff2fa4489acffca84d6c1a4a4
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-03 12:08:26 +0200

    #127 Add in price graph and out price graph indicator list plot

--- tools/data_visualiser.py
+++ tools/data_visualiser.py
@@ -27,16 +27,66 @@ class DataVisualiser:
         plt.show()
 
     @staticmethod
-    def show_candlesticks_dataframe_with_indicator(prices_data_frame, indicator_data_frame):
-        fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+    def show_candlesticks_dataframe_with_indicator(prices_data_frame, indicator_data_frame, in_graph=True):
+        if in_graph:
+            n_rows = 1
+        else:
+            n_rows = 2
 
-        DataVisualiser.get_plot_candlesticks_dataframe(prices_data_frame, ax1)
-        DataVisualiser.get_plot_indicator_dataframe(indicator_data_frame, ax2)
+        fig, axes = plt.subplots(nrows=n_rows, sharex=True)
+
+        if in_graph:
+            DataVisualiser.get_plot_candlesticks_dataframe(prices_data_frame, axes)
+            axe = fig.add_subplot(111, frameon=False)
+            axe.get_yaxis().set_ticks([])
+            DataVisualiser.add_indicator_to_plot(indicator_data_frame, axe)
+        else:
+            DataVisualiser.get_plot_candlesticks_dataframe(prices_data_frame, axes[0])
+            DataVisualiser.add_indicator_to_plot(indicator_data_frame, axes[1])
 
         fig.autofmt_xdate()
         fig.tight_layout()
 
         plt.show()
+
+    @staticmethod
+    def show_candlesticks_dataframe_with_indicators(prices_data_frame, indicator_data_frames):
+        count_in_graph = 1
+        for indicator_data_frame in indicator_data_frames:
+            if not indicator_data_frame["in_graph"]:
+                count_in_graph += 1
+
+        fig, axes = plt.subplots(nrows=count_in_graph, sharex=True)
+
+        if count_in_graph > 1:
+            DataVisualiser.get_plot_candlesticks_dataframe(prices_data_frame, axes[0])
+            axes[0].set_title('Symbol prices')
+        else:
+            DataVisualiser.get_plot_candlesticks_dataframe(prices_data_frame, axes)
+            axes.set_title('Symbol prices')
+
+        row = 1
+        for indicator_data_frame in indicator_data_frames:
+            if indicator_data_frame["in_graph"]:
+                axe = fig.add_subplot(count_in_graph * 100 + 10 + row, frameon=False)
+                axe.get_yaxis().set_ticks([])
+                DataVisualiser.add_indicator_to_plot(indicator_data_frame["data"],
+                                                     axe)
+            else:
+                DataVisualiser.add_indicator_to_plot(indicator_data_frame["data"],
+                                                     axes[row],
+                                                     indicator_data_frame["title"])
+                row += 1
+
+        fig.autofmt_xdate()
+        fig.tight_layout()
+        plt.show()
+
+    @staticmethod
+    def add_indicator_to_plot(indicator_data_frame, ax, title=None):
+        DataVisualiser.get_plot_indicator_dataframe(indicator_data_frame, ax)
+        if title is not None:
+            ax.set_title('Axe {0}'.format(title))
 
     @staticmethod
     def get_plot_candlesticks_dataframe(data_frame, ax):
