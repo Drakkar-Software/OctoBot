630306f68d8826a2f648c4accacb17d1cdb569bd
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-04 11:50:32 +0200

    #129 Display history of evaluator value on web interface

--- evaluator/evaluator_matrix.py
+++ evaluator/evaluator_matrix.py
@@ -1,4 +1,8 @@
-from config.cst import EvaluatorMatrixTypes, CONFIG_DEBUG_MATRIX_HISTORY, CONFIG_DEBUG_MATRIX_HISTORY_FILE
+import copy
+import time
+
+from config.cst import EvaluatorMatrixTypes, CONFIG_DEBUG_MATRIX_HISTORY
+from tools.evaluators_util import check_valid_eval_note
 
 
 class EvaluatorMatrix:
@@ -11,6 +15,11 @@ class EvaluatorMatrix:
             EvaluatorMatrixTypes.STRATEGIES: {}
         }
 
+        if EvaluatorMatrixHistory.enabled(self.config):
+            self.matrix_history = EvaluatorMatrixHistory()
+        else:
+            self.matrix_history = None
+
     # ---- getters and setters----
     def set_eval(self, matrix_type, evaluator_name, value, time_frame=None):
         if evaluator_name not in self.matrix[matrix_type]:
@@ -21,21 +30,47 @@ class EvaluatorMatrix:
         else:
             self.matrix[matrix_type][evaluator_name] = value
 
-        if EvaluatorMatrixHistory.enabled(self.config):
-            EvaluatorMatrixHistory.update_history(self.matrix)
+        if self.matrix_history is not None:
+            self.matrix_history.update_history(self.matrix)
 
     def get_type_evals(self, matrix_type):
         return self.matrix[matrix_type]
 
+    @staticmethod
+    def get_eval_note(matrix, matrix_type, evaluator_name, time_frame=None):
+        if matrix_type in matrix and evaluator_name in matrix[matrix_type]:
+            if time_frame is not None:
+                if time_frame in matrix[matrix_type][evaluator_name]:
+                    eval_note = matrix[matrix_type][evaluator_name][time_frame]
+                    if check_valid_eval_note(eval_note):
+                        return eval_note
+            else:
+                eval_note = matrix[matrix_type][evaluator_name]
+                if check_valid_eval_note(eval_note):
+                    return eval_note
+        return None
+
     def get_matrix(self):
         return self.matrix
 
+    def get_matrix_history(self):
+        return self.matrix_history
+
 
 class EvaluatorMatrixHistory:
-    @staticmethod
-    def update_history(matrix):
-        with open(CONFIG_DEBUG_MATRIX_HISTORY_FILE, "a") as file:
-            file.write(str(matrix))
+    def __init__(self):
+        self.matrix_history = []
+
+    def get_history(self):
+        return self.matrix_history
+
+    def update_history(self, matrix):
+        self.matrix_history.append({
+            "matrix": copy.deepcopy(matrix),
+            "timestamp": time.time()
+        })
+        # with open(CONFIG_DEBUG_MATRIX_HISTORY_FILE, "a") as file:
+        #     json.dump(matrix, file)
 
     @staticmethod
     def enabled(config):
