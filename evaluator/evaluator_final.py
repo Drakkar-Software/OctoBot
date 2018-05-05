197426d558f85f3ebca608318867b1cdc07cfbba
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-05 11:03:39 +0200

    FIx risk logic with market orders

--- evaluator/evaluator_final.py
+++ evaluator/evaluator_final.py
@@ -21,10 +21,10 @@ class FinalEvaluator(AsynchronousServer):
         self.logger = logging.getLogger(self.__class__.__name__)
 
         # If final_eval not is < X_THRESHOLD --> state = X
-        self.VERY_LONG_THRESHOLD = -0.75
+        self.VERY_LONG_THRESHOLD = -0.95
         self.LONG_THRESHOLD = -0.25
         self.NEUTRAL_THRESHOLD = 0.25
-        self.SHORT_THRESHOLD = 0.75
+        self.SHORT_THRESHOLD = 0.95
         self.RISK_THRESHOLD = 0.2
 
         self.notifier = EvaluatorNotification(self.config)
@@ -115,7 +115,7 @@ class FinalEvaluator(AsynchronousServer):
     def _create_state(self):
         risk = self.symbol_evaluator.get_trader(self.exchange).get_risk()
 
-        if self.final_eval < self.VERY_LONG_THRESHOLD - (self.RISK_THRESHOLD * risk):
+        if self.final_eval < self.VERY_LONG_THRESHOLD + (self.RISK_THRESHOLD * risk):
             self._set_state(EvaluatorStates.VERY_LONG)
 
         elif self.final_eval < self.LONG_THRESHOLD + (self.RISK_THRESHOLD * risk):
@@ -124,7 +124,7 @@ class FinalEvaluator(AsynchronousServer):
         elif self.final_eval < self.NEUTRAL_THRESHOLD - (self.RISK_THRESHOLD * risk):
             self._set_state(EvaluatorStates.NEUTRAL)
 
-        elif self.final_eval < self.SHORT_THRESHOLD + (self.RISK_THRESHOLD * risk):
+        elif self.final_eval < self.SHORT_THRESHOLD - (self.RISK_THRESHOLD * risk):
             self._set_state(EvaluatorStates.SHORT)
 
         else:
