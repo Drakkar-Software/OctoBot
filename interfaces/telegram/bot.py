6e7ebca099ccad934dccf92d1c48fefc9c418d15
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-06 23:59:32 +0200

    #87 telegram bot handle /start command

--- interfaces/telegram/bot.py
+++ interfaces/telegram/bot.py
@@ -1,13 +1,40 @@
 import logging
 
+from telegram.ext import CommandHandler, MessageHandler, Filters
+
 from config.cst import *
 
 
 class TelegramApp:
-    def __init__(self, config, telegram_service):
+    def __init__(self, config, telegram_service, telegram_updater):
         self.config = config
         self.telegram_service = telegram_service
+        self.telegram_updater = telegram_updater
+        self.dispatcher = self.telegram_updater.dispatcher
         self.logger = logging.getLogger(self.__class__.__name__)
+
+        self.add_handlers()
+
+        # Start the Bot
+        self.telegram_updater.start_polling()
+
+    def add_handlers(self):
+        self.dispatcher.add_handler(CommandHandler("start", self.command_start))
+
+        # log all errors
+        self.dispatcher.add_error_handler(self.command_error)
+
+        # TEST : unhandled messages
+        self.dispatcher.add_handler(MessageHandler(Filters.text, self.echo))
+
+    def command_start(self, bot, update):
+        self.telegram_service.send_message("I'm here !")
+
+    def command_error(self, bot, update):
+        self.telegram_service.send_message("Failed to perform this command : {0}".format(update.message.text))
+
+    def echo(self, bot, update):
+        update.message.reply_text(update.message.text)
 
     @staticmethod
     def enable(config):
--- services/telegram_service.py
+++ services/telegram_service.py
@@ -1,4 +1,5 @@
 import telegram
+from telegram.ext import Updater
 
 from config.cst import *
 from interfaces.telegram.bot import TelegramApp
@@ -12,6 +13,7 @@ class TelegramService(AbstractService):
         self.telegram_api = None
         self.chat_id = None
         self.telegram_app = None
+        self.telegram_updater = None
 
     @staticmethod
     def is_setup_correctly(config):
@@ -28,14 +30,25 @@ class TelegramService(AbstractService):
                 token=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_TELEGRAM][CONFIG_TOKEN])
 
         if not self.telegram_app:
+            if not self.telegram_updater:
+                self.telegram_updater = Updater(
+                    token=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_TELEGRAM][CONFIG_TOKEN])
+
             if TelegramApp.is_enabled(self.config):
-                self.telegram_app = TelegramApp(self.config, self)
+                self.telegram_app = TelegramApp(self.config, self, self.telegram_updater)
 
     def get_type(self):
         return CONFIG_TELEGRAM
 
     def get_endpoint(self):
         return self.telegram_api
+
+    def get_updater(self):
+        return self.telegram_updater
+
+    def stop_telegram_app(self):
+        if self.telegram_updater:
+            self.telegram_updater.stop()
 
     def has_required_configuration(self):
         return CONFIG_CATEGORY_SERVICES in self.config \
