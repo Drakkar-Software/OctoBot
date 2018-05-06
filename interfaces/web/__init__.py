5b6530dacd3bb787c3e99c4234742a667dc70c40
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-06 17:17:36 +0200

    Improve web bot data model

--- interfaces/web/__init__.py
+++ interfaces/web/__init__.py
@@ -1,12 +1,9 @@
 import copy
-
-import dash
+import logging
 import time
 
+import dash
 import flask
-import logging
-
-import cryptobot
 
 server_instance = flask.Flask(__name__)
 app_instance = dash.Dash(__name__, sharing=True, server=server_instance, url_base_pathname='/dashboard')
@@ -19,7 +16,13 @@ log.setLevel(logging.ERROR)
 
 matrix_history = []
 symbol_data_history = {}
-portfolio_value_history = []
+portfolio_value_history = {
+    "real_value": [],
+    "simulated_value": [],
+    "timestamp": []
+}
+
+TIME_AXIS_TITLE = "Time"
 
 
 def __init__(bot, config):
@@ -38,15 +41,13 @@ def add_to_matrix_history(matrix):
 
 
 def add_to_portfolio_value_history(real_value, simulated_value):
-    portfolio_value_history.append({
-        "real_value": real_value,
-        "simulated_value": simulated_value,
-        "timestamp": time.time()
-    })
+    portfolio_value_history["real_value"].append(real_value)
+    portfolio_value_history["simulated_value"].append(simulated_value)
+    portfolio_value_history["timestamp"].append(time.time())
 
 
 def add_to_symbol_data_history(symbol, data, time_frame):
-    if not symbol in symbol_data_history:
+    if symbol not in symbol_data_history:
         symbol_data_history[symbol] = []
 
     symbol_data_history[symbol].append({
@@ -68,7 +69,7 @@ def get_symbol_data_history():
     return symbol_data_history
 
 
-def get_bot() -> cryptobot.CryptoBot:
+def get_bot():
     return bot_instance
 
 
