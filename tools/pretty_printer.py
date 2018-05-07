7e5a984ba39ddc09ae32f0372bad9a70728d50d3
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-07 12:33:39 +0200

    Fix notifications with tests

--- tools/pretty_printer.py
+++ tools/pretty_printer.py
@@ -5,8 +5,14 @@ class PrettyPrinter:
     @staticmethod
     def open_order_pretty_printer(order):
         currency, market = order.get_currency_and_market()
+
+        try:
+            order_type_name = OrderConstants.TraderOrderTypeClasses[order.get_order_type()].__name__
+        except KeyError:
+            order_type_name = order.get_order_type().__class__.__name__
+
         return "{0} (on {1}) : {2} {3} at {4} {5}".format(
-            OrderConstants.TraderOrderTypeClasses[order.get_order_type()].__name__,
+            order_type_name,
             order.get_exchange().get_name(),
             round(order.get_origin_quantity(), 7),
             currency,
