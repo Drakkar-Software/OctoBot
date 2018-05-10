cf7d4812563b1feb661f80ad3c3de6435332cd74
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-10 16:25:13 +0200

    #141 [Trading] Implement real order management

--- trading/trader/trader_simulator.py
+++ trading/trader/trader_simulator.py
@@ -1,6 +1,4 @@
 from config.cst import CONFIG_ENABLED_OPTION
-from trading.trader.order import OrderConstants
-from trading.trader.order_notifier import OrderNotifier
 from trading.trader.trader import Trader
 
 """ TraderSimulator has a role of exchange response simulator
@@ -17,35 +15,3 @@ class TraderSimulator(Trader):
             return True
         else:
             return False
-
-    def create_order(self, order_type, symbol, current_price, quantity, price=None, stop_price=None, linked_to=None):
-        self.logger.info("Order creation : {0} | {1} | Price : {2} | Quantity : {3}".format(symbol,
-                                                                                            order_type,
-                                                                                            price,
-                                                                                            quantity))
-
-        # create new order instance
-        order_class = OrderConstants.TraderOrderTypeClasses[order_type]
-        order = order_class(self)
-
-        # manage order notifier
-        if linked_to is None:
-            order_notifier = OrderNotifier(self.config, order)
-        else:
-            order_notifier = linked_to.get_order_notifier()
-
-        order.new(order_type, symbol, current_price, quantity, price, stop_price, order_notifier)
-
-        # update the availability of the currency in the portfolio
-        with self.portfolio as pf:
-            pf.update_portfolio_available(order, is_new_order=True)
-
-        # notify order manager of a new open order
-        self.order_manager.add_order_to_list(order)
-
-        # if this order is linked to another (ex : a sell limit order with a stop loss order)
-        if linked_to is not None:
-            linked_to.add_linked_order(order)
-            order.add_linked_order(linked_to)
-
-        return order
