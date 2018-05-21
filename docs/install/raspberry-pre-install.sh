22812c27b6e1c80b5335841f7ddd4b51ce728605
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-21 18:59:47 +0200

    #166 Fix CI with functional testing

--- docs/install/raspberry-pre-install.sh
+++ docs/install/raspberry-pre-install.sh
@@ -4,4 +4,4 @@ sudo apt install -y build-essential libc6-dev libncurses5-dev libncursesw5-dev l
 
 sudo add-apt-repository ppa:jonathonf/python-3.6
 sudo apt update
-sudo apt install -y python3 python3-pip
+sudo apt install -y python3 python3-pip python-tk
