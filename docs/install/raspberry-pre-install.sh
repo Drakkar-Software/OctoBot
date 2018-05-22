a30d3ad6488b2527aeee6fc54addaf34da30dd8a
Herklos <herklos.dev@protonmail.com>
2018-05-22 15:01:16 +0200

    Update raspberry-pre-install.sh

--- docs/install/raspberry-pre-install.sh
+++ docs/install/raspberry-pre-install.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install -y build-essential libc6-dev
-sudo apt-get install -y libncurses5-dev libncursesw5-dev libreadline6-dev
-sudo apt-get install -y libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev
-sudo apt-get install -y libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-
-sudo apt install -y libxml2-dev libxslt1-dev libxslt-dev
-sudo apt install -y libjpeg-dev zlib1g-dev libpng12-dev
-sudo apt install -y libffi-dev
-
-sudo add-apt-repository ppa:jonathonf/python-3.6
-sudo apt update
-sudo apt install -y python3 python3-pip
+add-apt-repository ppa:jonathonf/python-3.6
+apt update
+apt install -y python3 python3-pip
