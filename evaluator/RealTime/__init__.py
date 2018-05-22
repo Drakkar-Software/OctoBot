23d79f1c28b9fa86e3f918836e4ba2ec9adabc77
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-22 19:22:19 +0200

    #171 prepare package manager prototype

--- evaluator/RealTime/__init__.py
+++ evaluator/RealTime/__init__.py
@@ -1,4 +1,1 @@
-from .realtime_evaluator import *
-from .instant_fluctuations_evaluator import *
-from .orderbook_evaluator import *
 from .Advanced import *
--- evaluator/Strategies/__init__.py
+++ evaluator/Strategies/__init__.py
@@ -1,3 +1,2 @@
 from .strategies_evaluator import *
-from .mixed_strategies_evaluator import *
 from .Advanced import *
