3ac344192aa4e88862963e5a8f29fccc354dc6c5
Herklos <herklos.dev@protonmail.com>
2018-05-22 15:01:38 +0200

    Update linux_installer.sh

--- docs/install/linux_installer.sh
+++ docs/install/linux_installer.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 apt install -y python3 python3-pip python3-tk -y
+bash ./docs/install/linux_dependencies.sh
 bash ./docs/install/install-ta-lib.sh
 bash ./docs/install/install-matplotlib.sh
