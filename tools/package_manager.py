c4d0b5d07e584b4cf7aebd49ef862641dd719090
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-05-23 23:37:38 +0200

    #176 fixed error message

--- tools/package_manager.py
+++ tools/package_manager.py
@@ -165,15 +165,18 @@ class PackageManager:
                             except Exception:
                                 self.logger.error("Installation failed for module '{0}'".format(component))
                         else:
+                            found = False
                             for advanced_package in self.advanced_package_list:
                                 if component in advanced_package:
+                                    found = True
                                     package_description = advanced_package[PACKAGE_DESCRIPTION]
                                     package_localisation = package_description[PACKAGE_DESCRIPTION_LOCALISATION]
                                     is_url = package_description[PACKAGE_DESCRIPTION_IS_URL]
                                     try:
                                         self.install_module(advanced_package, component, package_localisation, is_url,
                                                             EVALUATOR_ADVANCED_FOLDER)
+                                        break
                                     except Exception:
                                         self.logger.error("Installation failed for module '{0}'".format(component))
-                                else:
-                                    self.logger.warning("Cannot find installation for package '{0}'".format(component))
+                            if not found:
+                                self.logger.error("Cannot find installation for module '{0}'".format(component))
