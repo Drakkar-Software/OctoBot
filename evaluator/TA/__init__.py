41c6094839099765908e55e58f0b0ae5904c6e34
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-05-23 23:06:27 +0200

    #176 package manager now handles custom packages (url or local paths)

--- evaluator/TA/__init__.py
+++ evaluator/TA/__init__.py
@@ -1,3 +1,3 @@
 from .TA_evaluator import *
-from .Advanced import *
 from .Default import *
+from .Advanced import *
--- evaluator/Util/__init__.py
+++ evaluator/Util/__init__.py
@@ -1,4 +1,4 @@
 from .abstract_util import *
 from .advanced_manager import *
-from .Advanced import *
 from .Default import *
+from .Advanced import *
