2dd02a830d4e63b9436238a2c38b0e70387732eb
Paul Bouquet <paul.bouquet@gmail.com>
2018-05-26 14:36:47 +0200

    Improve windows installer

--- docs/install/windows/windows_installer.bat
+++ docs/install/windows/windows_installer.bat
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
+@echo off
 REM VARS
 set bot_location="../../../"
+set config_path=config
+set python_cmd=python
 
 REM REQUIREMENTS
-echo "Installing dependencies..."
+echo **Installing dependencies...**
 REM ARCH AMD64
-python -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win_amd64.whl
-python -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win_amd64.whl
+%python_cmd% -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win_amd64.whl
+%python_cmd% -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win_amd64.whl
 
 REM ARCH WIN32
-python -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win32.whl
-python -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win32.whl
+%python_cmd% -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win32.whl
+%python_cmd% -m pip install wheels/Twisted-18.4.0-cp36-cp36m-win32.whl
 
 REM BOT INSTALL
 cd %bot_location%
 
 REM BOT REQUIREMENTS
-echo "Installing requirements..."
-python -m pip install -r requirements.txt
+echo **Installing requirements...**
+%python_cmd% -m pip install -r requirements.txt
+
+REM CONFIGURATION
+echo **Set default configuration...**
+cd %config_path%
+copy default_config.json config.json
+copy default_evaluator_config.json evaluator_config.json
 
 REM BOT MODULES INSTALL
-echo "Installing modules..."
-python start.py -p install all
+echo **Installing modules...**
+cd ..
+%python_cmd% start.py -p install all
