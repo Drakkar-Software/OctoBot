0c16f7c4e1a85b732c03166169e78a2bc5731a64
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-05-31 22:51:44 +0200

    #139 added timeframes in evaluator matrix tests

--- tests/unit_tests/evaluator_tests/test_evaluator_matrix.py
+++ tests/unit_tests/evaluator_tests/test_evaluator_matrix.py
@@ -2,7 +2,7 @@ import math
 
 from evaluator.evaluator_matrix import EvaluatorMatrix
 from tests.test_utils.config import load_test_config
-from config.cst import EvaluatorMatrixTypes
+from config.cst import EvaluatorMatrixTypes, TimeFrames
 
 
 def _get_tools():
@@ -21,13 +21,15 @@ def test_init():
 
 
 def test_set_get_eval():
-    matrix_inst, matrix, config = _get_tools()
+    time_frames = [None, TimeFrames.ONE_HOUR]
     values = [1, 1.02, math.pi, math.nan]
-    for value in values:
-        for matrix_type in EvaluatorMatrixTypes:
-            key = "{}{}".format(matrix_type, value)
-            matrix_inst.set_eval(matrix_type, key, value)
-            if math.isnan(value):
-                assert matrix_inst.get_eval_note(matrix, matrix_type, key) is None
-            else:
-                assert matrix_inst.get_eval_note(matrix, matrix_type, key) == value
+    for time_frame in time_frames:
+        matrix_inst, matrix, config = _get_tools()
+        for value in values:
+            for matrix_type in EvaluatorMatrixTypes:
+                key = "{}{}".format(matrix_type, value)
+                matrix_inst.set_eval(matrix_type, key, value, time_frame)
+                if math.isnan(value):
+                    assert matrix_inst.get_eval_note(matrix, matrix_type, key, time_frame) is None
+                else:
+                    assert matrix_inst.get_eval_note(matrix, matrix_type, key, time_frame) == value
