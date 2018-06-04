f789222ddc022b0c71b9f9fa2ec17adf19f92ff9
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-04 23:46:52 +0200

    #215 prepare changelog

--- trading/trader/modes/abstract_trading_mode.py
+++ trading/trader/modes/abstract_trading_mode.py
@@ -1,4 +1,8 @@
+import os
 from abc import *
+
+from config.config import load_config
+from config.cst import CONFIG_FILE_EXT, CONFIG_TRADING, CONFIG_TRADER, CONFIG_TRADER_MODES
 
 
 class AbstractTradingMode:
@@ -8,10 +12,34 @@ class AbstractTradingMode:
         self.config = config
 
         self.decider = None
+        self.trading_config = None
         self.creators = {}
 
     def set_decider(self, decider):
         self.decider = decider
+
+    @classmethod
+    def get_name(cls):
+        return cls.__name__
+
+    @classmethod
+    def get_config_file_name(cls):
+        return "{0}/{1}/{2}/{3}".format(CONFIG_TRADING, CONFIG_TRADER, CONFIG_TRADER_MODES,
+                                        cls.get_name() + CONFIG_FILE_EXT)
+
+    def load_config(self):
+        config_file = self.get_config_file_name()
+        # try with this class name
+        if os.path.isfile(config_file):
+            self.trading_config = load_config(config_file)
+
+        # set default config if nothing found
+        if not self.trading_config:
+            self.set_default_config()
+
+    # to implement in subclasses if config necessary
+    def set_default_config(self):
+        pass
 
     def add_creator(self, creator, creator_key=None):
         if not creator_key:
