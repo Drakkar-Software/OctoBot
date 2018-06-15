3c53619cf8aa711d68199cc7584e8ca4315b42cc
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-15 20:07:42 +0200

    Update linux installer

--- docs/install/linux_installer.sh
+++ docs/install/linux_installer.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+echo **Installing dependencies...**
 apt install -y wget python3 python3-dev python3-pip python3-tk -y
 bash ./docs/install/linux_dependencies.sh
+
+echo **Installing requirements...**
 bash ./docs/install/install-ta-lib.sh
+
+echo **Set default configuration...**
+cd %config_path%
+cp default_config.json ../config.json
+
+REM BOT MODULES INSTALL
+echo **Installing modules...**
+cd ..
+python3 start.py -p install all
