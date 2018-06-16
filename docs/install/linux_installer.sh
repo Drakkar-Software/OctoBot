7f80ce7a50fe711b45423ab244cf689b002a35fa
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-16 13:29:22 +0200

    Update setup

--- docs/install/linux_installer.sh
+++ docs/install/linux_installer.sh
@@ -3,15 +3,13 @@ echo **Installing dependencies...**
 apt install -y wget python3 python3-dev python3-pip python3-tk -y
 bash ./docs/install/linux_dependencies.sh
 
-#echo **Installing requirements...**
-#python3 -m pip install -r requirements.txt
-#python3 -m pip install tulipy
+echo **Installing requirements...**
+python3 -m pip install -r requirements.txt
 
-#echo **Set default configuration...**
-#cd %config_path%
-#cp default_config.json ../config.json
+echo **Set default configuration...**
+cd %config_path%
+cp default_config.json ../config.json
 
-# BOT MODULES INSTALL
-#echo **Installing modules...**
-#cd ..
-#python3 start.py -p install all
+echo **Installing modules...**
+cd ..
+python3 start.py -p install all
