c11b06f42c1a72fa5b5f4ac37dc9e518bc0bba24
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-18 22:11:18 +0200

    Ensure code quality

--- tools/tentacle_manager.py
+++ tools/tentacle_manager.py
@@ -230,7 +230,7 @@ class TentacleManager:
                 if action == TentacleManagerActions.UPDATE:
                     # uninstall module
                     # TODO : rollback to previous version
-                    req_package, description, localisation, is_url, destination = self._get_package_in_lists(module_name)
+                    req_package, _, localisation, is_url, destination = self._get_package_in_lists(module_name)
                     if req_package:
                         self.process_module(TentacleManagerActions.UNINSTALL, req_package, module_name,
                                             localisation, is_url, destination)
@@ -238,7 +238,7 @@ class TentacleManager:
                 elif action == TentacleManagerActions.INSTALL:
                     # uninstall module and requirements
                     for module in applied_modules:
-                        req_package, description, localisation, is_url, destination = self._get_package_in_lists(module)
+                        req_package, _, localisation, is_url, destination = self._get_package_in_lists(module)
                         if req_package:
                             self.process_module(TentacleManagerActions.UNINSTALL, req_package, module,
                                                 localisation, is_url, destination)
@@ -383,7 +383,7 @@ class TentacleManager:
             for component in commands:
 
                 component = self._check_format(component)
-                package, description, localisation, is_url, destination = self._get_package_in_lists(component)
+                package, _, localisation, is_url, destination = self._get_package_in_lists(component)
 
                 if package:
                     try:
