67623c41481cf8d54f47b137d23e6dff1e0282bf
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-19 00:18:40 +0200

    #237 [Exchange Simulator] Fix with new symbol data

--- backtesting/collector/data_parser.py
+++ backtesting/collector/data_parser.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from config.cst import CONFIG_DATA_COLLECTOR_PATH
+from config.cst import CONFIG_DATA_COLLECTOR_PATH, PriceIndexes
 
 
 class DataCollectorParser:
@@ -9,8 +9,31 @@ class DataCollectorParser:
     def parse(file):
         if os.path.isfile(CONFIG_DATA_COLLECTOR_PATH + file):
             with open(CONFIG_DATA_COLLECTOR_PATH + file) as file_to_parse:
-                file_content = json.loads(file_to_parse.read())
+                file_content = DataCollectorParser.merge_arrays(json.loads(file_to_parse.read()))
         else:
             with open(file) as file_to_parse:
-                file_content = json.loads(file_to_parse.read())
+                file_content = DataCollectorParser.merge_arrays(json.loads(file_to_parse.read()))
         return file_content
+
+    @staticmethod
+    def merge_arrays(arrays):
+        time_frames_data = {}
+        for time_frame in arrays:
+            data = arrays[time_frame]
+            time_frames_data[time_frame] = []
+            for i in range(len(data[PriceIndexes.IND_PRICE_TIME.value])):
+                time_frames_data[time_frame].insert(i, [])
+                time_frames_data[time_frame][i].insert(PriceIndexes.IND_PRICE_CLOSE.value,
+                                                       data[PriceIndexes.IND_PRICE_CLOSE.value][i])
+                time_frames_data[time_frame][i].insert(PriceIndexes.IND_PRICE_OPEN.value,
+                                                       data[PriceIndexes.IND_PRICE_OPEN.value][i])
+                time_frames_data[time_frame][i].insert(PriceIndexes.IND_PRICE_HIGH.value,
+                                                       data[PriceIndexes.IND_PRICE_HIGH.value][i])
+                time_frames_data[time_frame][i].insert(PriceIndexes.IND_PRICE_LOW.value,
+                                                       data[PriceIndexes.IND_PRICE_LOW.value][i])
+                time_frames_data[time_frame][i].insert(PriceIndexes.IND_PRICE_TIME.value,
+                                                       data[PriceIndexes.IND_PRICE_TIME.value][i])
+                time_frames_data[time_frame][i].insert(PriceIndexes.IND_PRICE_VOL.value,
+                                                       data[PriceIndexes.IND_PRICE_VOL.value][i])
+
+        return time_frames_data
