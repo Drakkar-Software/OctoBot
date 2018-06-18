ce7c5c181e23004bc7cf6eeedd2d77d509a94079
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-18 23:36:45 +0200

    #237 [Exchange Simulator] Fix datacollector

--- backtesting/collector/exchange_collector.py
+++ backtesting/collector/exchange_collector.py
@@ -77,7 +77,7 @@ class ExchangeDataCollector(threading.Thread):
                 self.file_contents[symbol][time_frame.value] = self.exchange.get_symbol_prices(symbol,
                                                                                                time_frame,
                                                                                                limit=None,
-                                                                                               return_list=False)
+                                                                                               return_list=True)
                 self.time_frame_update[symbol][time_frame] = time.time()
             self._update_file(symbol)
 
@@ -98,7 +98,7 @@ class ExchangeDataCollector(threading.Thread):
                         result_df = self.exchange.get_symbol_prices(symbol,
                                                                     time_frame,
                                                                     limit=1,
-                                                                    return_list=False)[0]
+                                                                    return_list=True)[0]
 
                         self.file_contents[symbol][time_frame.value].append(result_df)
                         self._data_updated = True
