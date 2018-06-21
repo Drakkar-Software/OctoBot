227dfb8554d353be8a43782b64a0951720333a95
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-21 23:14:54 +0200

    Fix subportfolio

--- trading/trader/sub_portfolio.py
+++ trading/trader/sub_portfolio.py
@@ -49,13 +49,13 @@ class SubPortfolio(Portfolio):
         self.is_relative = is_relative
 
     def update_portfolio(self, order):
+        super().update_portfolio(order)
         self.parent_portfolio.update_portfolio(order)
-        self.update_from_parent()
 
     def update_portfolio_available(self, order, is_new_order=False):
+        super().update_portfolio_available(order, is_new_order=is_new_order)
         self.parent_portfolio.update_portfolio_available(order, is_new_order=is_new_order)
-        self.update_from_parent()
 
     def reset_portfolio_available(self, reset_currency=None, reset_quantity=None):
+        super().reset_portfolio_available(reset_currency=reset_currency, reset_quantity=reset_quantity)
         self.parent_portfolio.reset_portfolio_available(reset_currency=reset_currency, reset_quantity=reset_quantity)
-        self.update_from_parent()
