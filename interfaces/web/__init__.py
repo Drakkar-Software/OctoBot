629a804544ec3c62117a3b7e954451dca2df7d04
Paul Bouquet <paul.bouquet@gmail.com>
2018-06-27 21:32:29 +0200

    #268 Add small loading when a new page is rendered

--- interfaces/web/__init__.py
+++ interfaces/web/__init__.py
@@ -113,3 +113,4 @@ def load_routes():
     from .flask_controller import backtesting
     from .flask_controller import tentacle_manager
     from .flask_controller import update
+    from .flask_controller import commands
