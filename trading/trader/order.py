3bddd85ab3d72e9369fda3653d5012d32a2e94ea
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-07-04 00:49:27 +0200

    fixed real trader cancel order synchro bug with portfolio

--- trading/trader/order.py
+++ trading/trader/order.py
@@ -154,6 +154,9 @@ class Order:
     def get_currency_total_fees(self):
         return self.currency_total_fees
 
+    def get_order_id(self):
+        return self.order_id
+
     def get_filled_quantity(self):
         return self.filled_quantity
 
