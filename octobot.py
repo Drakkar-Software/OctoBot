abc8945ea7b4b9a5ec54edc2a77d0cc347242c10
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-07-04 23:56:49 +0200

    #290 improved error messages readability

--- octobot.py
+++ octobot.py
@@ -166,7 +166,7 @@ class OctoBot:
                                                                                   real_time_ta_eval_list,
                                                                                   self.relevant_evaluators)
             else:
-                self.logger.error("{0} exchange is not supporting the required time frame: {1} for {2}.".
+                self.logger.error("{0} exchange is not supporting the required time frame: '{1}' for {2}.".
                                   format(exchange.get_name(), time_frame.value, symbol_evaluator.get_symbol()))
         self.symbol_time_frame_updater_threads.append(symbol_time_frame_updater_thread)
 
