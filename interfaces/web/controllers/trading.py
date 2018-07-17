19e133bfb6ea814d556ed672e36b459638bd296c
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-17 21:39:28 +0200

    #322 [Web interface] Create portfolio page

--- interfaces/web/controllers/trading.py
+++ interfaces/web/controllers/trading.py
@@ -1,11 +1,27 @@
 from flask import render_template
 
+from interfaces.trading_util import get_traders
 from interfaces.web import server_instance
+from trading.trader.portfolio import Portfolio
 
 
 @server_instance.route("/portfolio")
 def portfolio():
-    return render_template('portfolio.html')
+    total_portfolio = {}
+    traders = get_traders()
+
+    for trader in traders:
+        for currency, amounts in trader.get_portfolio().get_portfolio().items():
+            if currency in total_portfolio:
+                total_portfolio[currency][Portfolio.TOTAL] += amounts[Portfolio.TOTAL]
+                total_portfolio[currency][Portfolio.AVAILABLE] += amounts[Portfolio.AVAILABLE]
+            else:
+                total_portfolio[currency] = {
+                    Portfolio.TOTAL: amounts[Portfolio.TOTAL],
+                    Portfolio.AVAILABLE: amounts[Portfolio.AVAILABLE],
+                }
+
+    return render_template('portfolio.html', total_portfolio=total_portfolio)
 
 
 @server_instance.route("/orders")
