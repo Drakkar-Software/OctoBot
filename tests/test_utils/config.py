268915a3eba8f54ff3b6ef4fdd2c5133b480c004
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-18 20:05:52 +0200

    #326 Fix Cis

--- tests/test_utils/config.py
+++ tests/test_utils/config.py
@@ -2,7 +2,15 @@ from config.config import load_config
 from config.cst import CONFIG_EVALUATOR, TimeFrames, CONFIG_TIME_FRAME
 
 
+def init_config_time_frame_for_tests(config):
+    result = []
+    for time_frame in config[CONFIG_TIME_FRAME]:
+        result.append(TimeFrames(time_frame))
+    config[CONFIG_TIME_FRAME] = result
+
+
 def load_test_config():
     config = load_config("tests/static/config.json")
     config[CONFIG_EVALUATOR] = load_config("tests/static/evaluator_config.json", False)
+    init_config_time_frame_for_tests(config)
     return config
