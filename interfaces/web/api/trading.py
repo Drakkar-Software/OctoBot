79817c7bff88bbba893ea910c7d403351a8c2219
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-19 23:08:42 +0200

    #328 Fixes

--- interfaces/web/api/trading.py
+++ interfaces/web/api/trading.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+import json
 
 from interfaces.trading_util import get_open_orders
 from . import api
@@ -7,4 +7,5 @@ from . import api
 @api.route("/orders")
 def orders():
     real_open_orders, simulated_open_orders = get_open_orders()
-    return jsonify({"real_open_orders": real_open_orders, "simulated_open_orders": simulated_open_orders})
+
+    return json.dumps({"real_open_orders": real_open_orders, "simulated_open_orders": simulated_open_orders})
