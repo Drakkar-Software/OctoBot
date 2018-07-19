d942c78b886902499abb5196806a49ddff0be242
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-19 23:08:42 +0200

    #328 Fixes

--- interfaces/web/static/js/common/bot_connection.js
+++ interfaces/web/static/js/common/bot_connection.js
@@ -58,7 +58,7 @@ function send_and_interpret_bot_update(updated_data, update_url, dom_root_elemen
             window.console&&console.error(status);
             window.console&&console.error(error);
             if(typeof error_callback === "undefined") {
-                create_alert("danger", "Error when handling action: "+result.responseText+".", "");
+                create_alert("error", "Error when handling action: "+result.responseText+".", "");
             }
             else{
                 error_callback(updated_data, update_url, dom_root_element, result, status, error)
