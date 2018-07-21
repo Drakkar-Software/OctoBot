6c3a8b1d3e6edf12b8288ac2b4b4921dcd388630
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-21 09:57:51 +0200

    #311 #312 add real config to prototype

--- interfaces/web/controllers/configuration.py
+++ interfaces/web/controllers/configuration.py
@@ -1,7 +1,8 @@
 import ccxt
 from flask import render_template, request, jsonify
 
-from config.cst import CONFIG_EXCHANGES
+from config.cst import CONFIG_EXCHANGES, CONFIG_CATEGORY_SERVICES, CONFIG_CATEGORY_NOTIFICATION, \
+    CONFIG_TRADER, CONFIG_SIMULATOR, CONFIG_CRYPTO_CURRENCIES
 from interfaces import get_bot
 
 from interfaces.web import server_instance
@@ -28,8 +29,17 @@ def config():
                 return get_rest_reply('{"update": "ko"}', 500)
     else:
         g_config = get_bot().get_config()
+        user_exchanges = [e for e in g_config[CONFIG_EXCHANGES]]
         return render_template('config.html',
-                               ccxt_exchanges=ccxt.exchanges,
+
+                               config_exchanges=g_config[CONFIG_EXCHANGES],
+                               config_trader=g_config[CONFIG_TRADER],
+                               config_trader_simulator=g_config[CONFIG_SIMULATOR],
+                               config_notifications=g_config[CONFIG_CATEGORY_NOTIFICATION],
+                               config_services=g_config[CONFIG_CATEGORY_SERVICES],
+                               config_symbols=g_config[CONFIG_CRYPTO_CURRENCIES],
+
+                               ccxt_exchanges=list(set(ccxt.exchanges) - set(user_exchanges)),
                                services_list=get_services_list(),
                                symbol_list=get_symbol_list([exchange for exchange in g_config[CONFIG_EXCHANGES]]),
                                get_evaluator_config=get_evaluator_config,
