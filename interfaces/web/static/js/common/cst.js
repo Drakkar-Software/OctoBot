5370a43469454659c5cb7b950e6d588a43a6e35b
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-23 20:44:37 +0200

    Fix card decks

--- interfaces/web/static/js/common/cst.js
+++ interfaces/web/static/js/common/cst.js
@@ -8,21 +8,22 @@ var config_type_attr = "config_type";
 var config_root_class = "config-root";
 var config_container_class = "config-container";
 var config_element_class = "config-element";
+var config_default_value = "@default_value";
 
 // dom display classes
-var success_badge = "badge-success"
-var warning_badge = "badge-warning"
-var secondary_badge = "badge-secondary"
-var primary_badge = "badge-primary"
+var success_badge = "badge-success";
+var warning_badge = "badge-warning";
+var secondary_badge = "badge-secondary";
+var primary_badge = "badge-primary";
 
-var light_list_item = "list-group-item-light"
-var success_list_item = "list-group-item-success"
+var light_list_item = "list-group-item-light";
+var success_list_item = "list-group-item-success";
 
-var activation_pending = "Activation pending restart"
-var deactivation_pending = "Deactivation pending restart"
-var unsaved_setting = "Unsaved setting"
-var activated = "Activated"
-var deactivated = "Deactivated"
+var activation_pending = "Activation pending restart";
+var deactivation_pending = "Deactivation pending restart";
+var unsaved_setting = "Unsaved setting";
+var activated = "Activated";
+var deactivated = "Deactivated";
 
 // utility functions
 function log(text){
