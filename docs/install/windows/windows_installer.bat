ea71cec54dbb556cb6da4029bccdf1d78421fa55
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-28 14:54:11 +0200

    Update windows installer

--- docs/install/windows/windows_installer.bat
+++ docs/install/windows/windows_installer.bat
@@ -8,6 +8,12 @@ REM dependencies url
 set TWISTED_WIN32=https://github.com/Drakkar-Software/OctoBot/releases/download/0.0.12-alpha/Twisted-18.4.0-cp36-cp36m-win32.whl
 set TWISTED_WIN64=https://github.com/Drakkar-Software/OctoBot/releases/download/0.0.12-alpha/Twisted-18.4.0-cp36-cp36m-win_amd64.whl
 
+set CYTOOLZ_WIN32=https://github.com/Drakkar-Software/OctoBot/releases/download/0.1.5_3-beta/cytoolz-0.9.0.1-cp36-cp36m-win32.whl
+set CYTOOLZ_WIN64=https://github.com/Drakkar-Software/OctoBot/releases/download/0.1.5_3-beta/cytoolz-0.9.0.1-cp36-cp36m-win_amd64.whl
+
+set LRU_DICT_WIN32=https://github.com/Drakkar-Software/OctoBot/releases/download/0.1.5_3-beta/lru_dict-1.1.6-cp36-cp36m-win32.whl
+set LRU_DICT_WIN64=https://github.com/Drakkar-Software/OctoBot/releases/download/0.1.5_3-beta/lru_dict-1.1.6-cp36-cp36m-win_amd64.whl
+
 REM TEST INSTALL
 %python_cmd% --version 2>NUL
 if errorlevel 1 goto errorNoPython
@@ -16,9 +22,13 @@ REM REQUIREMENTS
 echo **Installing dependencies...**
 REM ARCH AMD64
 %python_cmd% -m pip install %TWISTED_WIN64%
+%python_cmd% -m pip install %LRU_DICT_WIN64%
+%python_cmd% -m pip install %CYTOOLZ_WIN64%
 
 REM ARCH WIN32
 %python_cmd% -m pip install %TWISTED_WIN32%
+%python_cmd% -m pip install %LRU_DICT_WIN32%
+%python_cmd% -m pip install %CYTOOLZ_WIN32%
 
 REM BOT INSTALL
 cd %bot_location%
