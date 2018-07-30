13381478706cff2e0475266651d76f8a4fbed997
Paul Bouquet <paul.bouquet@gmail.com>
2018-07-30 21:09:19 +0200

    #335 [Notification] Refactor notification-type system

--- interfaces/web/models/configuration.py
+++ interfaces/web/models/configuration.py
@@ -46,10 +46,12 @@ def update_global_config(new_config):
 
 def get_services_list():
     services = {}
+    services_names = []
     for service in AbstractService.__subclasses__():
         srv = service()
         services[srv.get_type()] = srv
-    return services
+        services_names.append(srv.get_name())
+    return services, services_names
 
 
 def get_symbol_list(exchanges):
