56c98bbf86cb136c35f08033693977689b92814d
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-04 15:03:11 +0200

    #367 remove numba test

--- trading/exchanges/exchange_symbol_data.py
+++ trading/exchanges/exchange_symbol_data.py
@@ -1,7 +1,7 @@
 import logging
 
 import numpy as np
-from numba import jit
+
 from config.cst import PriceIndexes
 
 
@@ -125,7 +125,6 @@ class CandleData:
         self.is_initialized = True
 
     # getters
-    @jit
     def get_symbol_close_candles(self, limit=None, return_list=False):
         if return_list:
             return self.extract_limited_data(self.close_candles_list, limit)
@@ -133,7 +132,6 @@ class CandleData:
             self.update_arrays()
             return self.extract_limited_data(self.close_candles_array, limit)
 
-    @jit
     def get_symbol_open_candles(self, limit=None, return_list=False):
         if return_list:
             return self.extract_limited_data(self.open_candles_list, limit)
@@ -141,7 +139,6 @@ class CandleData:
             self.update_arrays()
             return self.extract_limited_data(self.open_candles_array, limit)
 
-    @jit
     def get_symbol_high_candles(self, limit=None, return_list=False):
         if return_list:
             return self.extract_limited_data(self.high_candles_list, limit)
@@ -149,7 +146,6 @@ class CandleData:
             self.update_arrays()
             return self.extract_limited_data(self.high_candles_array, limit)
 
-    @jit
     def get_symbol_low_candles(self, limit=None, return_list=False):
         if return_list:
             return self.extract_limited_data(self.low_candles_list, limit)
@@ -157,7 +153,6 @@ class CandleData:
             self.update_arrays()
             return self.extract_limited_data(self.low_candles_array, limit)
 
-    @jit
     def get_symbol_time_candles(self, limit=None, return_list=False):
         if return_list:
             return self.extract_limited_data(self.time_candles_list, limit)
@@ -165,7 +160,6 @@ class CandleData:
             self.update_arrays()
             return self.extract_limited_data(self.time_candles_array, limit)
 
-    @jit
     def get_symbol_volume_candles(self, limit=None, return_list=False):
         if return_list:
             return self.extract_limited_data(self.volume_candles_list, limit)
@@ -173,7 +167,6 @@ class CandleData:
             self.update_arrays()
             return self.extract_limited_data(self.volume_candles_array, limit)
 
-    @jit
     def get_symbol_prices(self, limit=None, return_list=False):
         symbol_prices = [None]*len(PriceIndexes)
         symbol_prices[PriceIndexes.IND_PRICE_CLOSE.value] = self.get_symbol_close_candles(limit, return_list)
@@ -189,14 +182,12 @@ class CandleData:
             return np.array(symbol_prices)
 
     # setters
-    @jit
     def set_last_candle(self, last_candle_data):
         self.close_candles_list[-1] = last_candle_data[PriceIndexes.IND_PRICE_CLOSE.value]
         self.high_candles_list[-1] = last_candle_data[PriceIndexes.IND_PRICE_HIGH.value]
         self.low_candles_list[-1] = last_candle_data[PriceIndexes.IND_PRICE_LOW.value]
         self.volume_candles_list[-1] = last_candle_data[PriceIndexes.IND_PRICE_VOL.value]
 
-    @jit
     def set_all_candles(self, new_candles_data):
         self.close_candles_list = []
         self.open_candles_list = []
@@ -211,7 +202,6 @@ class CandleData:
         else:
             self.add_new_candle(new_candles_data)
 
-    @jit
     def change_current_candle(self, new_last_candle_data):
         if len(self.time_candles_list) >= SymbolData.MAX_CANDLES_COUNT:
             self.close_candles_list.pop(0)
@@ -225,7 +215,6 @@ class CandleData:
     def should_add_new_candle(self, new_open_time):
         return new_open_time not in self.time_candles_list
 
-    @jit
     def add_new_candle(self, new_candle_data):
         self.close_candles_list.append(new_candle_data[PriceIndexes.IND_PRICE_CLOSE.value])
         self.open_candles_list.append(new_candle_data[PriceIndexes.IND_PRICE_OPEN.value])
@@ -234,7 +223,6 @@ class CandleData:
         self.time_candles_list.append(new_candle_data[PriceIndexes.IND_PRICE_TIME.value])
         self.volume_candles_list.append(new_candle_data[PriceIndexes.IND_PRICE_VOL.value])
 
-    @jit
     def update_arrays(self):
         if self.time_candles_array is None or self.time_candles_array[-1] != self.time_candles_list[-1]:
             self.close_candles_array = self.convert_list_to_array(self.close_candles_list)
@@ -257,7 +245,6 @@ class CandleData:
             self.sanitize_last_candle(self.close_candles_array, self.high_candles_array, self.low_candles_array)
     
     @staticmethod
-    @jit
     def sanitize_last_candle(close_candle_data, high_candle_data, low_candle_data):
         close_last_candle = close_candle_data[-1]
         if low_candle_data[-1] > close_last_candle:
@@ -266,18 +253,15 @@ class CandleData:
             high_candle_data[-1] = close_last_candle
             
     @staticmethod
-    @jit
     def set_last_candle_arrays(list_updated, array_to_update):
         if array_to_update is not None:
             array_to_update[-1] = list_updated[-1]                  
             
     @staticmethod
-    @jit
     def convert_list_to_array(list_to_convert):
         return np.array(list_to_convert)
 
     @staticmethod
-    @jit
     def extract_limited_data(data, limit=None):
         if limit is None:
             return data
