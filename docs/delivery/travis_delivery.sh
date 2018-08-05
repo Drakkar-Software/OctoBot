8d784e78c97f2a023fbc5680f808aeb77553b40d
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-06 00:56:29 +0200

    Fix travis

--- docs/delivery/travis_delivery.sh
+++ docs/delivery/travis_delivery.sh
@@ -5,9 +5,12 @@ pyinstaller start.py
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git pull travis-builds
 git checkout travis-builds
+
 git add --force dist/
 git commit -a -m "TRAVIS : binary push"
+
 git remote remove origin
 git remote add origin https://${GH_TOKEN}@${GH_REF}.git
 git push origin HEAD:travis-builds
