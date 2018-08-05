a60c8c87539d73bcfc28bff3a3a7c2fdbb4ed60a
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-06 00:31:04 +0200

    Fix travis delivery

--- docs/delivery/travis_delivery.sh
+++ docs/delivery/travis_delivery.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+pyinstaller start.py
+
 LAST_COMMIT_MESSAGE="$(git log --no-merges -1 --pretty=%B)"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git checkout travis-builds
 git add --force dist/
 git commit -a -m "TRAVIS : binary push"
 git remote remove origin
