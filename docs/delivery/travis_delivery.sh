cc86ce8d11b7c553d0153eabfd481aced2262951
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-06 00:02:22 +0200

    Fix travis 4

--- docs/delivery/travis_delivery.sh
+++ docs/delivery/travis_delivery.sh
@@ -6,7 +6,7 @@ git config --global user.name "Travis CI"
 
 git checkout travis-builds
 git add --force dist/
-git commit -a -m "TRAVIS : test release"
+git commit -a -m "TRAVIS : binary push"
 git remote remove origin
 git remote add origin https://${GH_TOKEN}@${GH_REF}.git
-git push origin HEAD:$(GIT_BRANCH)
+git push origin HEAD:dev
