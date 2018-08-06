e40a1c7e92ad6b774ffff4c9bc4f16efd3adbec3
Herklos <herklos.dev@protonmail.com>
2018-08-06 16:13:12 +0200

    Update travis_delivery.sh

--- docs/delivery/travis_delivery.sh
+++ docs/delivery/travis_delivery.sh
@@ -10,6 +10,7 @@ git config --global user.name "Travis CI"
 
 git clone -b ${TRAVIS_BRANCH} --single-branch git://${GH_REPO} ${GH_REPO_NAME}
 
+rm -rf ./${GH_REPO_NAME}/${BUILD_DIR}
 cp -rf ./${BUILD_DIR} ./${GH_REPO_NAME}
 cd ./${GH_REPO_NAME}
 
