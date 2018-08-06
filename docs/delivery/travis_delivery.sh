98838269123162f8cea61fafcdfd6b147f925c61
Herklos <herklos.dev@protonmail.com>
2018-08-06 13:25:52 +0200

    Update travis_delivery.sh

--- docs/delivery/travis_delivery.sh
+++ docs/delivery/travis_delivery.sh
@@ -2,15 +2,20 @@
 
 pyinstaller start.py --onefile
 
+REPO="OctoBot"
+BUILD_DIR="dist"
+BRANCH_TO_PUSH="travis-builds"
+GH_REPO="github.com/${GH_REF}.git"
+MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git add --force dist/
+git clone git://${GH_REPO} ${BRANCH_TO_PUSH}
+cp -r ./${BUILD_DIR} ${REPO}/${BUILD_DIR}
+cd ${REPO}
+
+git add -f ./${BUILD_DIR}
 git commit -a -m "TRAVIS : binary push"
-
-git remote remove origin
 git remote add origin https://${GH_TOKEN}@${GH_REF}.git
-
-git checkout travis-builds
-git pull travis-builds
-git push origin
+git push origin ${BRANCH_TO_PUSH}
