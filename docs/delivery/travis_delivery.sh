0d1cf7b4ffc5cf814b3da323a858e49cd95e7547
Herklos <herklos.dev@protonmail.com>
2018-08-06 14:14:32 +0200

    Update travis_delivery.sh

--- docs/delivery/travis_delivery.sh
+++ docs/delivery/travis_delivery.sh
@@ -8,11 +8,14 @@ MESSAGE=$(git log --format=%B -n 1 $TRAVIS_COMMIT)
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git clone git://${GH_REPO} ${TRAVIS_BRANCH}
-cp -r ./${BUILD_DIR} ${TRAVIS_BRANCH}/${BUILD_DIR}
-cd ${TRAVIS_BRANCH}
+git clone -b ${TRAVIS_BRANCH} --single-branch git://${GH_REPO} ${REPO}
+
+cp -r ./${BUILD_DIR} ${REPO}/${BUILD_DIR}
+cd ${REPO}
 
 git add -f ./${BUILD_DIR}
 git commit -a -m "TRAVIS : binary push"
+
+git remote remove origin
 git remote add origin https://${GH_TOKEN}@${GH_REF}.git
 git push origin ${TRAVIS_BRANCH}
