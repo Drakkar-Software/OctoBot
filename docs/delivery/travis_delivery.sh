327d204077dabc08d5b0b06604e383c2ee3928fc
Herklos <herklos.dev@protonmail.com>
2018-08-06 13:07:01 +0200

    Update travis_delivery.sh

--- docs/delivery/travis_delivery.sh
+++ docs/delivery/travis_delivery.sh
@@ -5,13 +5,12 @@ pyinstaller start.py --onefile
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git checkout travis-builds
-
 git add --force dist/
 git commit -a -m "TRAVIS : binary push"
 
 git remote remove origin
 git remote add origin https://${GH_TOKEN}@${GH_REF}.git
-git pull travis-builds
 
+git checkout travis-builds
+git pull travis-builds
 git push origin
