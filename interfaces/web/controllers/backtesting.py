b9bb28817b6ec23c9e36838b46f48be681c9df08
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-08-07 23:18:09 +0200

    #360 fixed page loading error

--- interfaces/web/controllers/backtesting.py
+++ interfaces/web/controllers/backtesting.py
@@ -55,10 +55,13 @@ def data_collector():
             details = request.get_json()
             success, reply = collect_data_file(details["exchange"], details["symbol"])
         elif action_type == "import_data_file":
-            file = request.files['file']
-            name = secure_filename(request.files['file'].filename)
-            success, reply = save_data_file(name, file)
-            alert = {"success": success, "message": reply}
+            if request.files:
+                file = request.files['file']
+                name = secure_filename(request.files['file'].filename)
+                success, reply = save_data_file(name, file)
+                alert = {"success": success, "message": reply}
+            else:
+                alert = {}
             current_exchange = get_current_exchange()
 
             # here return template to force page reload because of file upload via input form
@@ -80,11 +83,10 @@ def data_collector():
                 exchange = request.args.get('exchange')
                 return jsonify(sorted(get_symbol_list([exchange])))
 
-        else:
-            current_exchange = get_current_exchange()
-            return render_template('data_collector.html',
-                                   data_files=get_data_files_with_description(),
-                                   ccxt_exchanges=sorted(get_full_exchange_list()),
-                                   current_exchange=get_current_exchange(),
-                                   full_symbol_list=sorted(get_symbol_list([current_exchange])),
-                                   alert={})
+        current_exchange = get_current_exchange()
+        return render_template('data_collector.html',
+                               data_files=get_data_files_with_description(),
+                               ccxt_exchanges=sorted(get_full_exchange_list()),
+                               current_exchange=get_current_exchange(),
+                               full_symbol_list=sorted(get_symbol_list([current_exchange])),
+                               alert={})
