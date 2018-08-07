91142a0e7e7611bd5b9738b38e14c2bb1a83d45a
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-07 23:52:42 +0200

    Fix graph color

--- tools/timestamp_util.py
+++ tools/timestamp_util.py
@@ -7,21 +7,12 @@ TIMEZONE_DELTA = datetime.fromtimestamp(_EPOCH) - datetime.utcfromtimestamp(_EPO
 
 def convert_timestamp_to_datetime(timestamp, format='%d/%m/%y %H:%M', force_timezone=False):
     if force_timezone:
-        timestamp = timestamp_to_local(timestamp)
+        timestamp += TIMEZONE_DELTA.seconds
     return datetime.fromtimestamp(timestamp).strftime(format)
 
 
 def convert_timestamps_to_datetime(timestamps, format='%d/%m/%y %H:%M', force_timezone=False):
     return [convert_timestamp_to_datetime(timestamp, format=format, force_timezone=force_timezone)
-            for timestamp in timestamps]
-
-
-def timestamp_to_local(timestamp):
-    return timestamp + TIMEZONE_DELTA.seconds
-
-
-def timestamps_to_local(timestamps):
-    return [timestamp_to_local(timestamp)
             for timestamp in timestamps]
 
 
