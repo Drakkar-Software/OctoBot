3d7e4cefb8afe7ec1d74310145079e3b71ccc896
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-07 23:27:06 +0200

    #368 Fix timez

--- tools/timestamp_util.py
+++ tools/timestamp_util.py
@@ -5,8 +5,15 @@ _EPOCH = time.time()
 TIMEZONE_DELTA = datetime.fromtimestamp(_EPOCH) - datetime.utcfromtimestamp(_EPOCH)
 
 
-def convert_timestamp_to_datetime(timestamp, format='%d/%m/%y %H:%M'):
-    return datetime.fromtimestamp(timestamp_to_local(timestamp)).strftime(format)
+def convert_timestamp_to_datetime(timestamp, format='%d/%m/%y %H:%M', force_timezone=False):
+    if force_timezone:
+        timestamp = timestamp_to_local(timestamp)
+    return datetime.fromtimestamp(timestamp).strftime(format)
+
+
+def convert_timestamps_to_datetime(timestamps, format='%d/%m/%y %H:%M', force_timezone=False):
+    return [convert_timestamp_to_datetime(timestamp, format=format, force_timezone=force_timezone)
+            for timestamp in timestamps]
 
 
 def timestamp_to_local(timestamp):
