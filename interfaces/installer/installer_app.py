2bd9e064d3d423841b29ea769a2dce08885fb110
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-10 18:07:15 +0200

    Fix binary final name and close on download

--- interfaces/installer/installer_app.py
+++ interfaces/installer/installer_app.py
@@ -32,6 +32,8 @@ class InstallerApp(TkApp):
         if to_max:
             self.progress["value"] = self.PROGRESS_MAX
             self.progress_label["text"] = f"{self.PROGRESS_MAX}%"
+
+            self.stop()
         else:
             self.progress["value"] += inc_size
             self.progress_label["text"] = f"{round(self.progress['value'], 2)}%"
