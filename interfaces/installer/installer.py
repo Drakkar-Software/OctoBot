42f2733ae62483458ec188935844a8d6b2df1851
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-12 23:56:34 +0200

    Installer improvements

--- interfaces/installer/installer.py
+++ interfaces/installer/installer.py
@@ -2,11 +2,15 @@ import glob
 import json
 import logging
 import os
+import subprocess
+from distutils.version import LooseVersion
+from subprocess import PIPE
+from time import sleep
 
 import requests
 
 from config.cst import CONFIG_FILE, PROJECT_NAME, GITHUB_API_CONTENT_URL, GITHUB_REPOSITORY, GITHUB_RAW_CONTENT_URL, \
-    VERSION_DEV_PHASE, DEFAULT_CONFIG_FILE, LOGGING_CONFIG_FILE
+    VERSION_DEV_PHASE, DEFAULT_CONFIG_FILE, LOGGING_CONFIG_FILE, DeliveryPlatformsName, TENTACLES_PATH
 
 FOLDERS_TO_CREATE = ["logs"]
 FILES_TO_DOWNLOAD = [
@@ -28,14 +32,21 @@ FILES_TO_DOWNLOAD = [
 
 GITHUB_LATEST_RELEASE_URL = f"{GITHUB_API_CONTENT_URL}/repos/{GITHUB_REPOSITORY}/releases/latest"
 
+LIB_FILES_DOWNLOAD_PROGRESS_SIZE = 5
+CREATE_FOLDERS_PROGRESS_SIZE = 5
+BINARY_DOWNLOAD_PROGRESS_SIZE = 75
+TENTACLES_UPDATE_INSTALL_PROGRESS_SIZE = 15
+
 
 class Installer:
     def __init__(self, inst_app):
         self.installer_app = inst_app
 
         self.create_environment()
-        self.update_binary()
-        self.update_tentacles()
+        binary_path = self.update_binary()
+        self.update_tentacles(binary_path)
+
+        sleep(5)
 
     def create_environment(self):
         logging.info(f"{PROJECT_NAME} is checking your environment...")
@@ -52,14 +63,14 @@ class Installer:
                 with open(file_name, "w") as new_file_from_dl:
                     new_file_from_dl.write(file_content)
 
-        self.installer_app.inc_progress(5)
+        self.installer_app.inc_progress(LIB_FILES_DOWNLOAD_PROGRESS_SIZE)
 
         # create folders
         for folder in FOLDERS_TO_CREATE:
             if not os.path.exists(folder) and folder:
                 os.makedirs(folder)
 
-        self.installer_app.inc_progress(5)
+        self.installer_app.inc_progress(CREATE_FOLDERS_PROGRESS_SIZE)
 
         logging.info(f"Your {PROJECT_NAME} environment is ready !")
 
@@ -67,8 +78,7 @@ class Installer:
         # parse latest release
         try:
             logging.info(f"{PROJECT_NAME} is checking for updates...")
-            latest_release_data = json.loads(requests.get(GITHUB_LATEST_RELEASE_URL).text)
-            latest_version = latest_release_data["tag_name"]
+            latest_release_data = self.get_latest_release_data()
 
             # try to found in current folder binary
             octobot_binaries = glob.glob(f'{PROJECT_NAME}*')
@@ -77,34 +87,61 @@ class Installer:
             if octobot_binaries:
                 logging.info(f"{PROJECT_NAME} installation found, analyzing...")
 
-                # TODO temp --> check if current version is <
-                self.download_binary(latest_release_data)
-            else:
-                self.download_binary(latest_release_data)
+                binary_path = next(iter(octobot_binaries))
+                last_release_version = latest_release_data["tag_name"]
+                current_bot_version = self.execute_command_on_current_bot(binary_path, ["--version"])
 
-            pass
+                if LooseVersion(current_bot_version) < LooseVersion(last_release_version):
+                    logging.info(f"Upgrading {PROJECT_NAME} : from {current_bot_version} to {last_release_version}...")
+                    return self.download_binary(latest_release_data, replace=True)
+                else:
+                    logging.info(f"Nothing to do : {PROJECT_NAME} is up to date")
+                    self.installer_app.inc_progress(BINARY_DOWNLOAD_PROGRESS_SIZE)
+                    return binary_path
+            else:
+                return self.download_binary(latest_release_data)
         except Exception as e:
             logging.error(f"Failed to download latest release data : {e}")
+            raise e
 
-    def download_binary(self, latest_release_data):
-        # TEMP Choose binary
-        binary = None
+    @staticmethod
+    def get_latest_release_data():
+        return json.loads(requests.get(GITHUB_LATEST_RELEASE_URL).text)
+
+    @staticmethod
+    def execute_command_on_current_bot(binary_path, commands):
+        cmd = [f"./{binary_path}"] + commands
+        return subprocess.Popen(cmd, stdout=PIPE).stdout.read().rstrip().decode()
+
+    @staticmethod
+    def get_asset_from_release_data(latest_release_data):
+        os_name = None
 
         # windows
         if os.name == 'nt':
-            binary = latest_release_data["assets"][1]
+            os_name = DeliveryPlatformsName.WINDOWS
 
         # linux
         if os.name == 'posix':
-            binary = latest_release_data["assets"][0]
+            os_name = DeliveryPlatformsName.LINUX
 
         # mac
         if os.name == 'mac':
-            pass
+            os_name = DeliveryPlatformsName.MAC
+
+        # search for corresponding release
+        for asset in latest_release_data["assets"]:
+            asset_name, _ = os.path.splitext(asset["name"])
+            if asset_name == f"{PROJECT_NAME}_{os_name.value}":
+                return asset
+
+        return None
+
+    def download_binary(self, latest_release_data, replace=False):
+        binary = self.get_asset_from_release_data(latest_release_data)
 
         final_size = binary["size"]
-        binary_progress_importance = 75
-        increment = (binary_progress_importance / (final_size / 1024))
+        increment = (BINARY_DOWNLOAD_PROGRESS_SIZE / (final_size / 1024))
 
         r = requests.get(binary["browser_download_url"], stream=True)
 
@@ -112,12 +149,27 @@ class Installer:
         path = f"{PROJECT_NAME}{binary_ext}"
 
         if r.status_code == 200:
+
+            if replace and os.path.isfile(path):
+                try:
+                    os.remove(path)
+                except OSError:
+                    logging.error(f"Can't remove old version binary : {e}")
+
             with open(path, 'wb') as f:
                 for chunk in r.iter_content(1024):
                     f.write(chunk)
                     self.installer_app.inc_progress(increment)
 
-    def update_tentacles(self):
-        # TODO : call current binary "-p install all"
-        # os.execl("", ["-p", "install", "all"])
-        self.installer_app.inc_progress(5, to_max=True)
+    def update_tentacles(self, binary_path):
+        # if install required
+        if not os.path.exists(TENTACLES_PATH):
+            self.execute_command_on_current_bot(binary_path, ["-p", "install", "all"])
+            logging.info(f"Tentacles : all default tentacles has been installed.")
+
+        # update
+        else:
+            self.execute_command_on_current_bot(binary_path, ["-p", "update", "all"])
+            logging.info(f"Tentacles : all default tentacles has been updated.")
+
+        self.installer_app.inc_progress(TENTACLES_UPDATE_INSTALL_PROGRESS_SIZE, to_max=True)
