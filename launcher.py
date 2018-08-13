656d3835843fa09a7b4976213424aae161522de4
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-13 21:34:09 +0200

    #373 Fix launcher

--- launcher.py
+++ launcher.py
@@ -2,25 +2,34 @@ import argparse
 import importlib
 import logging
 
-from config.cst import GITHUB_RAW_CONTENT_URL, GITHUB_REPOSITORY, LAUNCHER_FILE, LAUNCHER_PATH
-from interfaces.launcher import create_environment_file
-from interfaces.launcher.launcher_app import LauncherApp
+from config.cst import GITHUB_RAW_CONTENT_URL, GITHUB_REPOSITORY, LAUNCHER_PATH
+
+logging.basicConfig(level=logging.INFO)
+
+# should have VERSION_DEV_PHASE
+LAUNCHER_URL = f"{GITHUB_RAW_CONTENT_URL}/{GITHUB_REPOSITORY}/dev/{LAUNCHER_PATH}"
+
+LAUNCHER_FILES = ["launcher_app.py", "launcher_controller.py"]
+
+
+def update_launcher():
+    logging.info("Updating launcher...")
+    for file in LAUNCHER_FILES:
+        create_environment_file(f"{LAUNCHER_URL}/{file}", f"{LAUNCHER_PATH}/{file}")
+
+
+try:
+    from interfaces.launcher import launcher_app, launcher_controller, create_environment_file
+except ImportError:
+    update_launcher()
+    try:
+        # importlib.import_module("interfaces.launcher")
+        from interfaces.launcher import launcher_app, launcher_controller, create_environment_file
+    except ImportError as e:
+        logging.error(f"Failed to start launcher : {e}")
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
-    try:
-        from interfaces.launcher import launcher_controller
-    except ImportError:
-        # should have VERSION_DEV_PHASE
-        logging.info("Installing launcher...")
-        launcher_url = f"{GITHUB_RAW_CONTENT_URL}/{GITHUB_REPOSITORY}/dev/{LAUNCHER_PATH}/{LAUNCHER_FILE}"
-        create_environment_file(launcher_url, LAUNCHER_FILE)
-        try:
-            importlib.import_module("launcher")
-        except ImportError as e:
-            logging.error(f"Failed to start launcher : {e}")
-
     parser = argparse.ArgumentParser(description='OctoBot - Launcher')
     parser.add_argument('-v', '--version', help='show OctoBot Launcher current version',
                         action='store_true')
@@ -39,5 +48,5 @@ if __name__ == '__main__':
     elif args.update_launcher:
         pass
     else:
-        installer_app = LauncherApp()
+        installer_app = launcher_app.LauncherApp()
         installer = launcher_controller.Launcher(installer_app)
