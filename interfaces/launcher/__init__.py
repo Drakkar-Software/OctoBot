c98bbadec2aa94cc5124d9ee30bc22ba0e6fb3ff
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-13 22:17:30 +0200

    #373 Try to fix launcher

--- interfaces/launcher/__init__.py
+++ interfaces/launcher/__init__.py
@@ -1,46 +1,5 @@
-import os
-
-import requests
-
-from config.cst import GITHUB_RAW_CONTENT_URL, GITHUB_REPOSITORY, LAUNCHER_PATH
-
-# should have VERSION_DEV_PHASE
-from interfaces.launcher.launcher_app import LauncherApp
-from interfaces.launcher.launcher_controller import Launcher
-
-LAUNCHER_URL = f"{GITHUB_RAW_CONTENT_URL}/{GITHUB_REPOSITORY}/dev/{LAUNCHER_PATH}"
-
-LAUNCHER_FILES = ["launcher_app.py", "launcher_controller.py"]
+import logging
 
 LAUNCHER_VERSION = "1.0.0"
 
-
-def update_launcher():
-    logging.info("Updating launcher...")
-    for file in LAUNCHER_FILES:
-        create_launcher_files(f"{LAUNCHER_URL}/{file}", f"{LAUNCHER_PATH}/{file}")
-
-
-def create_launcher_files(file_to_dl, result_file_path):
-    file_content = requests.get(file_to_dl).text
-    directory = os.path.dirname(result_file_path)
-
-    if not os.path.exists(directory) and directory:
-        os.makedirs(directory)
-
-    file_name = result_file_path
-    if not os.path.isfile(file_name) and file_name:
-        with open(file_name, "w") as new_file_from_dl:
-            new_file_from_dl.write(file_content)
-
-
-def start_launcher(args):
-    if args.version:
-        print(LAUNCHER_VERSION)
-    elif args.update:
-        pass
-    elif args.update_launcher:
-        pass
-    else:
-        installer_app = LauncherApp()
-        installer = Launcher(installer_app)
+logging.basicConfig(level=logging.INFO)
--- launcher.py
+++ launcher.py
@@ -1,17 +1,31 @@
 import argparse
 import importlib
-import logging
+import os
+import sys
 
-logging.basicConfig(level=logging.INFO)
+import requests
 
-try:
-    from interfaces.launcher import *
-except ImportError:
-    update_launcher()
-    try:
-        importlib.import_module("interfaces.launcher")
-    except ImportError as e:
-        logging.error(f"Failed to start launcher : {e}")
+from config.cst import GITHUB_RAW_CONTENT_URL, GITHUB_REPOSITORY, LAUNCHER_PATH
+
+# should have VERSION_DEV_PHASE
+LAUNCHER_URL = f"{GITHUB_RAW_CONTENT_URL}/{GITHUB_REPOSITORY}/dev/{LAUNCHER_PATH}"
+
+LAUNCHER_FILES = ["__init__.py", "launcher_app.py", "launcher_controller.py"]
+
+sys.path.append(os.path.dirname(sys.executable))
+
+
+def create_launcher_files(file_to_dl, result_file_path):
+    file_content = requests.get(file_to_dl).text
+    directory = os.path.dirname(result_file_path)
+
+    if not os.path.exists(directory) and directory:
+        os.makedirs(directory)
+
+    file_name = result_file_path
+    if not os.path.isfile(file_name) and file_name:
+        with open(file_name, "w") as new_file_from_dl:
+            new_file_from_dl.write(file_content)
 
 
 if __name__ == '__main__':
@@ -25,4 +39,13 @@ if __name__ == '__main__':
                         action='store_true')
 
     args = parser.parse_args()
+
+    for file in LAUNCHER_FILES:
+        create_launcher_files(f"{LAUNCHER_URL}/{file}", f"{LAUNCHER_PATH}/{file}")
+
+    try:
+        from interfaces.launcher.launcher_app import *
+    except ImportError:
+        importlib.import_module("interfaces.launcher.launcher_app")
+
     start_launcher(args)
