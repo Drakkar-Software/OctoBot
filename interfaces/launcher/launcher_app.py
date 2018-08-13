91d10f49edc70f6a0e30b77e63c61b922bf6a1fc
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-13 20:14:44 +0200

    #373 prepare installer self install

--- interfaces/launcher/launcher_app.py
+++ interfaces/launcher/launcher_app.py
@@ -2,9 +2,16 @@ import logging
 import os
 from tkinter.ttk import Progressbar, Label
 
-from config.cst import PROJECT_NAME
+from config.cst import PROJECT_NAME, GITHUB_RAW_CONTENT_URL, GITHUB_REPOSITORY, VERSION_DEV_PHASE, LAUNCHER_FILE
 from interfaces.app_util import TkApp
-from interfaces.launcher.launcher import Installer
+from interfaces.launcher import create_environment_file
+
+try:
+    from interfaces.launcher.launcher import Launcher
+except ImportError:
+    # should have VERSION_DEV_PHASE
+    launcher_url = f"{GITHUB_RAW_CONTENT_URL}/{GITHUB_REPOSITORY}/dev/{LAUNCHER_FILE}",
+    create_environment_file(launcher_url, LAUNCHER_FILE)
 
 
 class LauncherApp(TkApp):
@@ -49,5 +56,24 @@ class LauncherApp(TkApp):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    installer_app = LauncherApp()
-    installer = Installer(installer_app)
+
+    parser = argparse.ArgumentParser(description='OctoBot - Launcher')
+    parser.add_argument('-v', '--version', help='show OctoBot Launcher current version',
+                        action='store_true')
+    parser.add_argument('-u', '--update', help='update OctoBot with the latest version available',
+                        action='store_true')
+
+    parser.add_argument('-l', '--update_launcher', help='update OctoBot Launcher with the latest version available',
+                        action='store_true')
+
+    args = parser.parse_args()
+
+    if args.version:
+        print(Launcher.get_version())
+    elif args.update:
+        pass
+    elif args.update_launcher:
+        pass
+    else:
+        installer_app = LauncherApp()
+        installer = Launcher(installer_app)
