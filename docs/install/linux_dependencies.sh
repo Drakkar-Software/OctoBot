0d4a7d266282211e1ad8e3dc0869b9cf5908429c
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-14 23:51:28 +0200

    Fix CI

--- docs/install/linux_dependencies.sh
+++ docs/install/linux_dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 apt update
-apt install -y build-essential libc6-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+apt install -y python3-tk build-essential libc6-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 apt install -y libxml2-dev libxslt1-dev libxslt-dev libjpeg-dev zlib1g-dev libpng12-dev libffi-dev
