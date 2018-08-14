b98c2c9dc369c53039621fc1e4ca552b0d572eca
Paul Bouquet <paul.bouquet@gmail.com>
2018-08-15 00:03:20 +0200

    Fix CI 3

--- interfaces/starting/__init__.py
+++ interfaces/starting/__init__.py
@@ -4,6 +4,8 @@ try:
     from interfaces.starting.tk_app import StartingApp
 except ModuleNotFoundError as e:
     logging.error(f"Can't find {e}, impossible to load GUI")
+except ImportError as e:
+    logging.error(f"Can't find {e}, impossible to load GUI")
 
 tk_app = None
 
