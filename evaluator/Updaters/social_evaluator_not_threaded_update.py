379e4de20ac90a5b3ce77b13a83deb1b7454c6d8
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-08-21 19:24:06 +0200

    #305 refactored social evaluator updater

--- evaluator/Updaters/social_evaluator_not_threaded_update.py
+++ evaluator/Updaters/social_evaluator_not_threaded_update.py
@@ -6,6 +6,12 @@ from config.cst import *
 
 
 class SocialEvaluatorNotThreadedUpdateThread(threading.Thread):
+
+    LAST_REFRESH_KEY = "last_refresh"
+    LAST_REFRESH_TIME_KEY = "last_refresh_time"
+    REFRESH_RATE_KEY = "refresh_rate"
+    EVALUATOR_INSTANCE_KEY = "social_evaluator_class_inst"
+
     def __init__(self, social_evaluator_list):
         super().__init__()
         self.social_evaluator_list = social_evaluator_list
@@ -24,31 +30,32 @@ class SocialEvaluatorNotThreadedUpdateThread(threading.Thread):
                 if CONFIG_REFRESH_RATE in social_eval.get_social_config():
                     self.social_evaluator_list_timers.append(
                         {
-                            "social_evaluator_class_inst": social_eval,
-                            "refresh_rate": social_eval.get_social_config()[CONFIG_REFRESH_RATE],
+                            self.EVALUATOR_INSTANCE_KEY: social_eval,
+                            self.REFRESH_RATE_KEY: social_eval.get_social_config()[CONFIG_REFRESH_RATE],
                             # force first refresh
-                            "last_refresh": social_eval.get_social_config()[CONFIG_REFRESH_RATE],
-                            "last_refresh_time": time.time()
+                            self.LAST_REFRESH_KEY: social_eval.get_social_config()[CONFIG_REFRESH_RATE],
+                            self.LAST_REFRESH_TIME_KEY: time.time()
                         })
                 else:
-                    self.logger.warning("Social evaluator {0} doesn't have a valid social config refresh rate."
-                                        .format(social_eval.__class__.__name__))
+                    self.logger.warning(f"Social evaluator {social_eval.get_name()} doesn't have a valid social config "
+                                        "refresh rate.")
 
     def run(self):
         if self.social_evaluator_list_timers:
             while self.keep_running:
                 for social_eval in self.social_evaluator_list_timers:
                     now = time.time()
-                    social_eval["last_refresh"] += now - social_eval["last_refresh_time"]
-                    social_eval["last_refresh_time"] = now
-                    if social_eval["last_refresh"] >= social_eval["refresh_rate"]:
-                        social_eval["last_refresh"] = 0
+                    social_eval[self.LAST_REFRESH_KEY] += now - social_eval[self.LAST_REFRESH_TIME_KEY]
+                    social_eval[self.LAST_REFRESH_TIME_KEY] = now
+                    if social_eval[self.LAST_REFRESH_KEY] >= social_eval[self.REFRESH_RATE_KEY]:
+                        social_eval[self.LAST_REFRESH_KEY] = 0
 
                         try:
-                            social_eval["social_evaluator_class_inst"].get_data()
-                            social_eval["social_evaluator_class_inst"].eval()
+                            social_eval[self.EVALUATOR_INSTANCE_KEY].get_data()
+                            social_eval[self.EVALUATOR_INSTANCE_KEY].eval()
                         except Exception as e:
-                            self.logger.error(f"Error during Social not threaded update eval() or get_data() "
-                                              f"{social_eval['social_evaluator_class_inst']} : {e}")
+                            self.logger.error(f"Error during Social not threaded update eval() or get_data() on "
+                                              f"{social_eval[self.EVALUATOR_INSTANCE_KEY].get_name()} for "
+                                              f"{social_eval[self.EVALUATOR_INSTANCE_KEY].get_symbol()} : {e}")
 
                 time.sleep(SOCIAL_EVALUATOR_NOT_THREADED_UPDATE_RATE)
