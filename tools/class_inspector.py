ab86a1f94bc66514ac8e2b8ec780983fbbdf5946
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-08-27 23:37:28 +0200

    #376 improved trading config error handling

--- tools/class_inspector.py
+++ tools/class_inspector.py
@@ -9,12 +9,15 @@ def evaluator_parent_inspection(element, parent):
     return element.get_parent_evaluator_classes(parent)
 
 
-def get_class_from_string(class_string, parent, module, parent_inspection=_default_parent_inspection):
+def get_class_from_string(class_string, parent, module, parent_inspection=_default_parent_inspection,
+                          error_when_not_found=False):
     if any(m[0] == class_string and
            hasattr(m[1], '__bases__') and
            parent_inspection(m[1], parent)
            for m in inspect.getmembers(module)):
         return getattr(module, class_string)
+    if error_when_not_found:
+        raise ModuleNotFoundError(f"Cant find {class_string} module")
     return None
 
 
