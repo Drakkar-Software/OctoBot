6c1b3f9395e9f60e5ef855a99c255c79b8864b17
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-09-02 12:56:49 +0200

    #359 fixed commit error

--- interfaces/web/controllers/trading.py
+++ interfaces/web/controllers/trading.py
@@ -35,7 +35,8 @@ def orders():
     real_open_orders, simulated_open_orders = get_open_orders()
     return render_template('orders.html',
                            real_open_orders=real_open_orders,
-                           simulated_open_orders=simulated_open_orders)
+                           simulated_open_orders=simulated_open_orders,
+                           pairs_with_status=get_currencies_with_status())
 
 
 @server_instance.route("/trades")
