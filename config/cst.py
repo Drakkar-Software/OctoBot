2ad0b843c87e66f537c1a11a2913813f7d120193
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-09-06 00:00:35 +0200

    updated changelog and version for open beta release

--- config/cst.py
+++ config/cst.py
@@ -2,7 +2,7 @@ from logging import WARNING
 from enum import Enum
 
 PROJECT_NAME = "OctoBot"
-SHORT_VERSION = "0.1.7"
+SHORT_VERSION = "0.2.0"
 MINOR_VERSION = "0"
 VERSION_DEV_PHASE = "beta"
 VERSION = f"{SHORT_VERSION}-{VERSION_DEV_PHASE}"
