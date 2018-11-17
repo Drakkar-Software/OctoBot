1f90295eb22bf76bc39b60a9aa021b8758dd8c01
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-11-17 16:26:52 +0100

    #421 clarified portfolio page

--- interfaces/web/static/js/components/portfolio.js
+++ interfaces/web/static/js/components/portfolio.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     const portfolioElem=$("#portfoliosCard");
     const url = portfolioElem.attr(update_url_attr);
     const referenceMarket = portfolioElem.attr("reference_market");
-    const chartTitle = "Holdings in "+referenceMarket;
+    const chartTitle = "Traded assets value in "+referenceMarket;
     $.get(url,function(data) {
         createPortfolioChart("real_portfolio_doughnutChart", data["real_portfolio_holdings"], chartTitle);
         createPortfolioChart("simulated_portfolio_doughnutChart", data["simulated_portfolio_holdings"], chartTitle);
