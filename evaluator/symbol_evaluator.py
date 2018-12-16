159a18d0dcf966f5f61636ebb67b68dfc784d10d
Herklos <herklos.dev@protonmail.com>
2018-12-16 23:29:33 +0100

    Coding style improvements
    Signed-off-by: Herklos <herklos.dev@protonmail.com>

--- evaluator/symbol_evaluator.py
+++ evaluator/symbol_evaluator.py
@@ -144,11 +144,9 @@ class SymbolEvaluator:
                 decider.add_to_queue()
 
     def get_deciders_are_busy(self):
-        for trading_mode in self.trading_mode_instances.values():
-            for decider in trading_mode.get_deciders(self.symbol):
-                if decider.has_something_to_do():
-                    return True
-        return False
+        return any(decider.has_something_to_do()
+                   for trading_mode in self.trading_mode_instances.values()
+                   for decider in trading_mode.get_deciders(self.symbol))
 
     def are_all_timeframes_initialized(self, exchange):
         return self.finalize_enabled_list[exchange.get_name()]
