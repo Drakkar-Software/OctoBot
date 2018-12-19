abec67fca0701d4d097ffcdf34c53d7ff0c69223
Herklos <herklos.dev@protonmail.com>
2018-12-19 01:04:57 +0100

    Add volume in Dockerfile
    Signed-off-by: Herklos <herklos.dev@protonmail.com>

--- docs/install/linux_dependencies.sh
+++ docs/install/linux_dependencies.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-apt update
-apt install -y build-essential libc6-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
-apt install -y libxml2-dev libxslt1-dev libxslt-dev libjpeg-dev zlib1g-dev libpng12-dev libffi-dev
-apt install -y wget python3 python3-dev python3-pip python3-tk -y
+BUILD_DEPS="build-essential libc6-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev"
+APPLICATION_DEPS="libxml2-dev libxslt1-dev libxslt-dev libjpeg-dev zlib1g-dev libffi-dev"
+PYTHON_DEPS="wget python3 python3-dev python3-pip python3-tk"
 
-# Install python tk if python installed
-if command -v python3 &>/dev/null; then
-    apt install -y python3-tk
-fi
+apt update && apt install -qq -y --no-install-recommends $BUILD_DEPS $APPLICATION_DEPS $PYTHON_DEPS
