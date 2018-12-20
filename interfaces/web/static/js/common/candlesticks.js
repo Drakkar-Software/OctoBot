35aadfa1007e119224ddb7b6ce28d7268af90627
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2018-12-20 19:45:58 +0100

    #475 added license in files

--- interfaces/web/static/js/common/candlesticks.js
+++ interfaces/web/static/js/common/candlesticks.js
@@ -1,3 +1,21 @@
+/*
+ * Drakkar-Software OctoBot
+ * Copyright (c) Drakkar-Software, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
 function get_symbol_price_graph(element_id, exchange_name, symbol, time_frame, backtesting=false, replace=false){
     const backtesting_enabled = backtesting ? "backtesting" : "live";
     const ajax_url = "/dashboard/currency_price_graph_update/"+ exchange_name +"/" + symbol + "/"
