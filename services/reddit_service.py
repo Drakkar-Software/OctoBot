643c46dd07e10211b5337beb9f7472b768178d48
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-01-12 21:29:14 +0100

    #539 split required config and default values in services

--- services/reddit_service.py
+++ services/reddit_service.py
@@ -21,7 +21,27 @@ from services.abstract_service import *
 
 
 class RedditService(AbstractService):
-    REQUIRED_CONFIG = {"client-id": "", "client-secret": "", "password": "", "username": ""}
+    CLIENT_ID = "client-id"
+    CLIENT_SECRET = "client-secret"
+    PASSWORD = "password"
+    USERNAME = "username"
+
+    REQUIRED_CONFIG = [CLIENT_ID, CLIENT_SECRET, PASSWORD, USERNAME]
+
+    # Used in configuration interfaces
+    CONFIG_FIELDS_DESCRIPTION = {
+        CLIENT_ID: "Your client ID.",
+        CLIENT_SECRET: "Your client ID secret.",
+        PASSWORD: "Your Reddit username.",
+        USERNAME: "Your Reddit password."
+    }
+    CONFIG_DEFAULT_VALUE = {
+        CLIENT_ID: "",
+        CLIENT_SECRET: "",
+        PASSWORD: "",
+        USERNAME: ""
+    }
+    HELP_PAGE = "https://github.com/Drakkar-Software/OctoBot/wiki/Reddit-interface#reddit-interface"
 
     def __init__(self):
         super().__init__()
@@ -35,11 +55,11 @@ class RedditService(AbstractService):
     async def prepare(self):
         if not self.reddit_api:
             self.reddit_api = \
-                praw.Reddit(client_id=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT]["client-id"],
-                            client_secret=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT]["client-secret"],
-                            password=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT]["password"],
+                praw.Reddit(client_id=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT][self.CLIENT_ID],
+                            client_secret=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT][self.CLIENT_SECRET],
+                            password=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT][self.PASSWORD],
                             user_agent='bot',
-                            username=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT]["username"])
+                            username=self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT][self.USERNAME])
 
     def get_type(self):
         return CONFIG_REDDIT
@@ -53,5 +73,5 @@ class RedditService(AbstractService):
                and self.check_required_config(self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT])
 
     def get_successful_startup_message(self):
-        return f"Successfully initialized using {self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT]['username']}" \
+        return f"Successfully initialized using {self.config[CONFIG_CATEGORY_SERVICES][CONFIG_REDDIT][self.USERNAME]}" \
                f" account.", True
