9d5120223bd6fbee9fba540c4c81cb7100a114f4
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-01-20 17:02:26 +0100

    restored .format in config.py and increased min python version check to 3.7

--- config/config.py
+++ config/config.py
@@ -26,25 +26,25 @@ from config import CONFIG_FILE, OCTOBOT_KEY, DEFAULT_CONFIG_FILE
 
 def load_config(config_file=CONFIG_FILE, error=True):
     logger = logging.getLogger("CONFIG LOADER")
-    basic_error = f"Error when load config file {config_file}"
+    basic_error = "Error when load config file {0}".format(config_file)
     try:
         with open(config_file) as json_data_file:
             config = json.load(json_data_file)
         return config
     except ValueError as e:
-        error_str = f"{basic_error} : json decoding failed ({e})"
+        error_str = "{0} : json decoding failed ({1})".format(basic_error, e)
         if error:
             raise Exception(error_str)
         else:
             logger.error(error_str)
     except IOError as e:
-        error_str = f"{basic_error} : file opening failed ({e})"
+        error_str = "{0} : file opening failed ({1})".format(basic_error, e)
         if error:
             raise Exception(error_str)
         else:
             logger.error(error_str)
     except Exception as e:
-        error_str = f"{basic_error} : {e}"
+        error_str = "{0} : {1}".format(basic_error, e)
         if error:
             raise Exception(error_str)
         else:
@@ -56,7 +56,7 @@ def init_config(config_file=CONFIG_FILE, from_config_file=DEFAULT_CONFIG_FILE):
     try:
         copyfile(from_config_file, config_file)
     except Exception as e:
-        raise Exception(f"Can't init config file {e}")
+        raise Exception("Can't init config file {0}".format(e))
 
 
 def is_config_empty(config_file=CONFIG_FILE):
