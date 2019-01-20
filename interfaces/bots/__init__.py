409d0b8bc61b111bb036b5621bb25b0b8adc3761
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-01-20 17:21:41 +0100

    merged multi line string

--- interfaces/bots/__init__.py
+++ interfaces/bots/__init__.py
@@ -17,9 +17,9 @@ from tools.logging.logging_util import get_logger
 
 LOGGER = get_logger(__name__)
 EOL = "\n"
-NO_TRADER_MESSAGE = "No trader is activated in my config/config.json file.\n" \
-                    "See https://github.com/Drakkar-Software/OctoBot/wiki if you need help with my configuration."
+NO_TRADER_MESSAGE = """No trader is activated in my config/config.json file.
+See https://github.com/Drakkar-Software/OctoBot/wiki if you need help with my configuration."""
 NO_CURRENCIES_MESSAGE = "No cryptocurrencies are in my config/config.json file.\n" \
                         "See https://github.com/Drakkar-Software/OctoBot/wiki/Configuration#cryptocurrencies " \
-                        "if you need help with my cryptocurrencies configuration."
+                        "if you need help with my cryptocurrencies configuration."""
 UNAUTHORIZED_USER_MESSAGE = "Hello, I dont talk to strangers."
