c4ad2daf4283c9ca35b88aad7e2a8fd4ee4b91c4
Herklos <herklos.dev@protonmail.com>
2019-01-26 15:25:01 +0100

    #595 remove pre-launcher

--- docs/install/linux_dependencies.sh
+++ docs/install/linux_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 BUILD_DEPS="build-essential libc6-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev"
 APPLICATION_DEPS="libxml2-dev libxslt1-dev libxslt-dev libjpeg-dev zlib1g-dev libffi-dev"
-PYTHON_DEPS="wget python3 python3-dev python3-pip python3-tk"
+PYTHON_DEPS="wget python3 python3-dev python3-pip"
 ADDITIONAL_DEPS="git"
 
 apt update && apt install -qq -y $BUILD_DEPS $APPLICATION_DEPS $PYTHON_DEPS $ADDITIONAL_DEPS
