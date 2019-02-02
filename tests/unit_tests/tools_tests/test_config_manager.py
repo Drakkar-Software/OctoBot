d5a35cfabba51c8652df0ec4d3a93af041bdffbb
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-02-02 12:47:36 +0100

    added tests

---
+++ tests/unit_tests/tools_tests/test_config_manager.py
@@ -0,0 +1,81 @@
+#  Drakkar-Software OctoBot
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+from config import DEFAULT_REFERENCE_MARKET, CONFIG_TRADING, CONFIG_TRADER_REFERENCE_MARKET
+from config.config import load_config
+from tools.config_manager import ConfigManager
+from tools.symbol_util import split_symbol
+
+
+def test_get_market_pair():
+    config = load_config("tests/static/config.json")
+
+    pair, inverted = ConfigManager.get_market_pair(config, config[CONFIG_TRADING][CONFIG_TRADER_REFERENCE_MARKET])
+    assert pair == ""
+    assert inverted is False
+
+    pair, inverted = ConfigManager.get_market_pair(config, "")
+    assert pair == ""
+    assert inverted is False
+
+    pair, inverted = ConfigManager.get_market_pair(config, "VEN")
+    assert pair == "VEN/BTC"
+    assert inverted is False
+
+    pair, inverted = ConfigManager.get_market_pair(config, "USDT")
+    assert pair == "BTC/USDT"
+    assert inverted is True
+
+    pair, inverted = ConfigManager.get_market_pair(config, "XBT")
+    assert pair == ""
+    assert inverted is False
+
+    # now change config reference market
+    config[CONFIG_TRADING][CONFIG_TRADER_REFERENCE_MARKET] = "USDT"
+
+    pair, inverted = ConfigManager.get_market_pair(config, "BTC")
+    assert pair == "BTC/USDT"
+    assert inverted is False
+
+    pair, inverted = ConfigManager.get_market_pair(config, "VEN")
+    assert pair == ""
+    assert inverted is False
+
+    config[CONFIG_TRADING].pop(CONFIG_TRADER_REFERENCE_MARKET)
+
+    # now use config/__init__.py reference market
+    pair, inverted = ConfigManager.get_market_pair(config, "ADA")
+    assert pair == "ADA/BTC"
+    assert split_symbol(pair)[1] == DEFAULT_REFERENCE_MARKET
+    assert inverted is False
+
+    config.pop(CONFIG_TRADING)
+    pair, inverted = ConfigManager.get_market_pair(config, "ADA")
+    assert pair == ""
+    assert inverted is False
---
+++ tests/unit_tests/tools_tests/test_dict_util.py
@@ -0,0 +1,53 @@
+#  Drakkar-Software OctoBot
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+from math import nan
+
+from tools.dict_util import get_value_or_default
+
+
+def test_get_value_or_default():
+    test_dict = {"a": 1, "b": 2, "c": 3}
+    assert get_value_or_default(test_dict, "b", default="") == 2
+    assert get_value_or_default(test_dict, "d", nan) is nan
+    assert get_value_or_default(test_dict, "d", default="") == ""
+    assert get_value_or_default(test_dict, "z") is None
