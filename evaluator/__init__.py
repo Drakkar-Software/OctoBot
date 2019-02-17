aa4fd45676ff8ea48296304707298fe7fa5a23f9
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-02-17 02:46:50 +0100

    #663 telegram dispatcher working

--- evaluator/__init__.py
+++ evaluator/__init__.py
@@ -14,6 +14,7 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 
+
 from typing import Dict, Union, NewType
 
 from config import EvaluatorMatrixTypes
