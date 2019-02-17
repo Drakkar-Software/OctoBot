de87d3261592e6e239f10e40a77299abd78e5880
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-02-17 21:45:45 +0100

    #663 added telegram wildcard option for channels and fixed an authorization bug

--- interfaces/bots/telegram/bot.py
+++ interfaces/bots/telegram/bot.py
@@ -101,7 +101,7 @@ class TelegramApp(InterfaceBot):
             message += "/version or /v: `Displays my current software version.`" + EOL
             message += "/help: `Displays this help.`"
             update.message.reply_markdown(message)
-        else:
+        elif TelegramApp._is_authorized_chat(update):
             update.message.reply_text(UNAUTHORIZED_USER_MESSAGE)
 
     @staticmethod
@@ -112,7 +112,7 @@ class TelegramApp(InterfaceBot):
     def command_start(_, update):
         if TelegramApp._is_valid_user(update):
             TelegramApp._send_message(update, InterfaceBot.get_command_start(markdown=True))
-        else:
+        elif TelegramApp._is_authorized_chat(update):
             TelegramApp._send_message(update, UNAUTHORIZED_USER_MESSAGE)
 
     @staticmethod
@@ -241,10 +241,14 @@ class TelegramApp(InterfaceBot):
         return InterfaceBot.is_enabled(config, associated_config=associated_config)
 
     @staticmethod
+    def _is_authorized_chat(update):
+        return update.effective_chat["type"] in TelegramApp.HANDLED_CHATS
+
+    @staticmethod
     def _is_valid_user(update, associated_config=CONFIG_INTERFACES_TELEGRAM):
 
         # only authorize users from a private chat
-        if update.effective_chat["type"] not in TelegramApp.HANDLED_CHATS:
+        if not TelegramApp._is_authorized_chat(update):
             return False
 
         update_username = update.effective_chat["username"]
