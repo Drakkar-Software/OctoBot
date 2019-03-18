cee9c34ba95526fb6181d71d44ab706bfc25805e
Werdor <werdor@>
2019-03-18 22:28:31 +0100

    Octobot.py refactor to octobot core

--- tests/functional_tests/launch_test/launch_test.py
+++ tests/functional_tests/launch_test/launch_test.py
@@ -14,13 +14,13 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 
-import pytest
 import asyncio
 
-from config import *
-from octobot import OctoBot
-from tests.test_utils.config import load_test_config
+import pytest
 
+from config import *
+from core.octobot import OctoBot
+from tests.test_utils.config import load_test_config
 
 # All test coroutines will be treated as marked.
 pytestmark = pytest.mark.asyncio
@@ -35,9 +35,9 @@ async def test_run_bot(event_loop):
                 "pairs": ["BTC/USDT"]
             }
     }
-    bot = OctoBot(config)
+    bot = OctoBot(config, ignore_config=True)
     bot.time_frames = [TimeFrames.ONE_MINUTE]
-    await bot.create_exchange_traders(ignore_config=True)
-    bot.create_evaluation_tasks()
+    await bot.initialize()
+    await asyncio.get_event_loop().run_in_executor(None, bot.start)
     await asyncio.sleep(5)
-    await asyncio.get_event_loop().run_in_executor(None, bot.stop_threads)
+    await asyncio.get_event_loop().run_in_executor(None, bot.stop)
