060a6b160f9af683c103bd9086e75293fcba6cb5
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-03-23 11:46:30 +0100

    #747 added format check and handling

--- backtesting/backtesting.py
+++ backtesting/backtesting.py
@@ -18,8 +18,8 @@ from tools.logging.logging_util import get_logger
 import os
 import time
 
-from config import CONFIG_ANALYSIS_ENABLED_OPTION, CONFIG_BACKTESTING, CONFIG_ENABLED_OPTION, PriceIndexes, \
-    CONFIG_CRYPTO_CURRENCIES, CONFIG_CRYPTO_PAIRS
+from config import CONFIG_ANALYSIS_ENABLED_OPTION, CONFIG_BACKTESTING, PriceIndexes, CONFIG_CRYPTO_CURRENCIES, \
+    CONFIG_CRYPTO_PAIRS
 from tools.pretty_printer import PrettyPrinter
 
 
