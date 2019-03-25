5bf6b68ba8ed1a95db814a6bc3468544b6f01121
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-03-26 00:12:29 +0100

    #731 initatialized persistence
    #731 update to use portfolio from logs
    
    
    #731 added tests
    
    
    removed merge error

--- trading/trader/trader_simulator.py
+++ trading/trader/trader_simulator.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 
-from config import CONFIG_ENABLED_OPTION, CONFIG_SIMULATOR, SIMULATOR_TRADER_STR
+from config import CONFIG_ENABLED_OPTION, CONFIG_SIMULATOR, SIMULATOR_TRADER_STR, SIMULATOR_CURRENT_PORTFOLIO
 from trading.trader.trader import Trader
 
 """ TraderSimulator has a role of exchange response simulator
@@ -26,12 +26,29 @@ class TraderSimulator(Trader):
     NO_HISTORY_MESSAGE = "Starting a fresh new trading simulation session using trader simulator initial portfolio " \
                          "in configuration."
 
-    def __init__(self, config, exchange, order_refresh_time=None):
+    def __init__(self, config, exchange, order_refresh_time=None, previous_state_manager=None):
         self.simulate = True
-        super().__init__(config, exchange, order_refresh_time)
+        super().__init__(config, exchange, order_refresh_time, previous_state_manager)
 
         self.trader_type_str = SIMULATOR_TRADER_STR
 
     @staticmethod
     def enabled(config):
         return config[CONFIG_SIMULATOR][CONFIG_ENABLED_OPTION]
+
+    def load_previous_state_if_any(self):
+        loaded_previous_state = self.previous_state_manager.has_previous_state(self.exchange)
+        if not self.previous_state_manager.should_initialize_data() and loaded_previous_state:
+            try:
+                self._print_previous_state_info()
+                self.loaded_previous_state = True
+            except Exception as e:
+                self.logger.warning(f"Error when loading trading history, will reset history. ({e})")
+                self.logger.exception(e)
+                self.previous_state_manager.reset_trading_history()
+        else:
+            self.logger.info(self.NO_HISTORY_MESSAGE)
+
+    def _print_previous_state_info(self):
+        current_portfolio = self.previous_state_manager.get_previous_state(self.exchange, SIMULATOR_CURRENT_PORTFOLIO)
+        self.logger.info(f"Resuming the previous trading session: current portfolio: {current_portfolio}")
