ae77fe8528f571ecec34b44310942d08754f9104
Herklos <herklos@protonmail.com>
2019-04-13 19:03:24 +0200

    [Version] 0.4.0

--- config/__init__.py
+++ config/__init__.py
@@ -19,9 +19,9 @@ from enum import Enum
 from typing import NewType, Any, Dict
 
 PROJECT_NAME = "OctoBot"
-SHORT_VERSION = "0.3.5"  # major.minor.revision
+SHORT_VERSION = "0.4.0"  # major.minor.revision
 PATCH_VERSION = ""  # patch : pX
-VERSION_DEV_PHASE = ""  # alpha : a / beta : b / release candidate : rc
+VERSION_DEV_PHASE = "a"  # alpha : a / beta : b / release candidate : rc
 VERSION_PHASE = ""  # XX
 VERSION = f"{SHORT_VERSION}{VERSION_DEV_PHASE}{VERSION_PHASE}"
 LONG_VERSION = f"{SHORT_VERSION}{PATCH_VERSION}{VERSION_DEV_PHASE}{VERSION_PHASE}"
