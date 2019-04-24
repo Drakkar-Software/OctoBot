128fb65aad3bcb78c7a9a5215245936125720273
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-04-24 18:40:04 +0200

    #811 now starts telegram interface by default

--- start.py
+++ start.py
@@ -162,7 +162,7 @@ def start_octobot(starting_args):
                     from interfaces.bots.telegram.bot import TelegramApp
                     from services import WebService
 
-                    TelegramApp.enable(config, starting_args.telegram)
+                    TelegramApp.enable(config, not starting_args.no_telegram)
                     WebService.enable(config, not starting_args.no_web)
 
                     update_config_with_args(starting_args, config)
@@ -235,8 +235,9 @@ def main(args=None):
                         action='store_true')
     parser.add_argument('-no', '--no_open_web', help="Don't automatically open web interface.",
                         action='store_true')
-    parser.add_argument('-t', '--telegram', help='Start OctoBot with telegram interface. Required to listen to '
-                                                 'telegram signals.',
+    parser.add_argument('-nt', '--no-telegram', help='Start OctoBot without telegram interface, even if telegram '
+                                                     'credentials are in config. Telegram interface is required to '
+                                                     'listen to telegram signals and discuss with OctoBot on telegram.',
                         action='store_true')
     parser.add_argument('--encrypter', help="Start the exchange api keys encrypter. This tool is useful to manually add"
                                             " exchanges configuration in your config.json without using any interface "
