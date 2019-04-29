54dcdbd8cb55b175c04aad7e24109b9a133a21c3
Herklos <herklos.dev@protonmail.com>
2019-04-29 22:36:07 +0200

    [Channels][Personnal Data] Create balance and orders channels

--- core/channels/channel.py
+++ core/channels/channel.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 from abc import abstractmethod, ABCMeta
-from typing import Dict, List
+from typing import Dict
 
 from config import CONSUMER_CALLBACK_TYPE
 from core.channels.channel_instances import ChannelInstances
