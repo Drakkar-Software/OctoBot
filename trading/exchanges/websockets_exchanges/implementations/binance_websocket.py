588d71ba09f2c6fbfa65b2b33d6e9c13788327ad
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-05-12 15:21:18 +0200

    fixed usdX bug in ws

--- trading/exchanges/websockets_exchanges/implementations/binance_websocket.py
+++ trading/exchanges/websockets_exchanges/implementations/binance_websocket.py
@@ -290,9 +290,6 @@ class BinanceWebSocketClient(AbstractWebSocket):
         pass
 
     def _adapt_symbol(self, symbol):
-        if "USD" in symbol and "USDT" not in symbol:
-            symbol += "T"
-
         if "/" in symbol:
             symbol = merge_symbol(symbol)
 
