8a35c00d257388cec77565a59ac0ba9cdf17b213
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-08-26 23:40:16 +0200

    #968 add not found and internal error handlers

--- interfaces/web/controllers/errors.py
+++ interfaces/web/controllers/errors.py
@@ -15,16 +15,16 @@
 #  License along with this library.
 
 
-def load_routes():
-    from . import errors
-    from . import tentacles
-    from . import backtesting
-    from . import commands
-    from . import configuration
-    from . import home
-    from . import dashboard
-    from . import trading
-    from . import logs
-    from . import interface_settings
-    from . import community
-    from . import terms
+from flask import render_template
+
+from interfaces.web import server_instance
+
+
+@server_instance.errorhandler(404)
+def not_found(_):
+    return render_template("404.html")
+
+
+@server_instance.errorhandler(500)
+def internal_error(_):
+    return render_template("500.html")
