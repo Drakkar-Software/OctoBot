8f70f3a9cad999903058b1310c57ab109b05359f
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-09-02 00:31:02 +0200

    remove default config installation

--- docs/install/linux_installer.sh
+++ docs/install/linux_installer.sh
@@ -8,10 +8,5 @@ echo **Installing requirements...**
 python3 -m pip install -r pre_requirements.txt
 python3 -m pip install -r requirements.txt
 
-echo **Set default configuration...**
-cd $config_path
-cp default_config.json ../config.json
-
 echo **Installing modules...**
-cd ..
 python3 start.py -p install all
