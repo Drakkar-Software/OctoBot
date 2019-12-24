6df19dc2263673c7ddb067faca35f666f3f3ee63
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-12-24 02:14:44 +0100

    add service creation

--- octobot/evaluator_factory.py
+++ octobot/evaluator_factory.py
@@ -16,6 +16,7 @@
 
 from octobot_commons.logging.logging_util import get_logger
 from octobot_evaluators.api import create_matrix_channels, create_all_type_evaluators, initialize_evaluators
+from octobot_services.api.dispatchers import create_dispatcher_factory
 from octobot_trading.exchanges.exchanges import Exchanges
 from tools.logger import init_evaluator_chan_logger
 
@@ -34,11 +35,14 @@ class EvaluatorFactory:
         self.symbol_tasks_manager = {}
         self.symbol_evaluator_list = {}
         self.cryptocurrency_evaluator_list = {}
-        self.dispatchers_list = []
         self.social_eval_tasks = []
         self.real_time_eval_tasks = []
 
+        # Dispatcher instances, used by evaluators like twitter, telegram, ...
+        self.dispatcher_list = []
+
     async def initialize(self):
+        self._create_dispatchers()
         await initialize_evaluators(self.octobot.config)
         await create_matrix_channels()
 
@@ -50,6 +54,10 @@ class EvaluatorFactory:
                                              exchange_configuration.symbols,
                                              exchange_configuration.time_frames)
         await init_evaluator_chan_logger()
+
+    def _create_dispatchers(self):
+        dispatcher_factory = create_dispatcher_factory(self.octobot.config, self.octobot.task_manager.async_loop)
+        self.dispatcher_list = dispatcher_factory.create_all()
 
     # def _check_required_evaluators(self):
     #     if self.symbol_tasks_manager:
--- octobot/initializer.py
+++ octobot/initializer.py
@@ -13,7 +13,8 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-
+from octobot_backtesting.api.backtesting import is_backtesting_enabled
+from octobot_services.api.services import create_service_factory
 from tools import get_logger
 
 
@@ -31,6 +32,24 @@ class Initializer:
     async def create(self):
         # initialize tools
         self.__init_metrics()
+        await self._create_services()
+
+    async def _create_services(self):
+        service_factory = create_service_factory(self.octobot.config)
+        service_list = service_factory.get_available_services()
+        backtesting_enabled = is_backtesting_enabled(self.octobot.config)
+        for service_class in service_list:
+            await self._create_service_if_relevant(service_factory, service_class, backtesting_enabled)
+
+    async def _create_service_if_relevant(self, service_factory, service_class, backtesting_enabled):
+        service_instance = service_class()
+        if self._is_service_relevant(service_instance, backtesting_enabled):
+            service_instance.is_backtesting_enabled = backtesting_enabled
+            await service_factory.create_service(service_instance)
+
+    def _is_service_relevant(self, service, backtesting_enabled):
+        return service.get_is_enabled(self.octobot.config) and \
+                (not backtesting_enabled or service.BACKTESTING_ENABLED)
 
     def __init_metrics(self):
         pass  # TODO
