39854f58efaf8509210e48dc7e7398eef5881370
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-12-25 01:58:36 +0100

    replace service creation by interface creation

--- octobot/initializer.py
+++ octobot/initializer.py
@@ -13,8 +13,10 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+from config import PROJECT_NAME, LONG_VERSION
 from octobot_backtesting.api.backtesting import is_backtesting_enabled
-from octobot_services.api.services import create_service_factory
+from octobot_interfaces.api.interfaces import create_interface_factory, initialize_global_project_data
+from octobot_interfaces.base.abstract_interface import AbstractInterface
 from tools import get_logger
 
 
@@ -29,27 +31,32 @@ class Initializer:
         # Logger
         self.logger = get_logger(self.__class__.__name__)
 
+        self.interface_list = []
+
     async def create(self):
         # initialize tools
         self.__init_metrics()
-        await self._create_services()
+        await self._create_interfaces()
 
-    async def _create_services(self):
-        service_factory = create_service_factory(self.octobot.config)
-        service_list = service_factory.get_available_services()
+    async def _create_interfaces(self):
+        # do not overwrite data in case of inner bots init (backtesting)
+        if AbstractInterface.bot is None:
+            initialize_global_project_data(self.octobot, PROJECT_NAME, LONG_VERSION)
+        interface_factory = create_interface_factory(self.octobot.config)
+        interface_list = interface_factory.get_available_interfaces()
         backtesting_enabled = is_backtesting_enabled(self.octobot.config)
-        for service_class in service_list:
-            await self._create_service_if_relevant(service_factory, service_class, backtesting_enabled)
+        for interface_class in interface_list:
+            await self._create_interface_if_relevant(interface_factory, interface_class, backtesting_enabled)
 
-    async def _create_service_if_relevant(self, service_factory, service_class, backtesting_enabled):
-        service_instance = service_class()
-        if self._is_service_relevant(service_instance, backtesting_enabled):
-            service_instance.is_backtesting_enabled = backtesting_enabled
-            await service_factory.create_service(service_instance)
+    async def _create_interface_if_relevant(self, interface_factory, interface_class, backtesting_enabled):
+        if self._is_service_relevant(interface_class.REQUIRED_SERVICE, backtesting_enabled):
+            interface_instance = await interface_factory.create_interface(interface_class, backtesting_enabled)
+            interface_instance.initialize(backtesting_enabled)
+            self.interface_list.append(interface_instance)
 
-    def _is_service_relevant(self, service, backtesting_enabled):
-        return service.get_is_enabled(self.octobot.config) and \
-                (not backtesting_enabled or service.BACKTESTING_ENABLED)
+    def _is_service_relevant(self, service_class, backtesting_enabled):
+        return service_class.get_is_enabled(self.octobot.config) and \
+                (not backtesting_enabled or service_class.BACKTESTING_ENABLED)
 
     def __init_metrics(self):
         pass  # TODO
