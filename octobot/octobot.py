dfa8bec07d5961b93dda778c0ce42bbd2ec4dea2
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-01-05 22:35:11 +0100

    update startup_config and edited_config for interfaces

--- octobot/octobot.py
+++ octobot/octobot.py
@@ -25,7 +25,9 @@ from octobot.interface_factory import InterfaceFactory
 from octobot.task_manager import TaskManager
 from octobot_commons.enums import MarkdownFormat
 from octobot_commons.logging.logging_util import get_logger
+from octobot_evaluators.constants import CONFIG_EVALUATOR
 from octobot_notifications.api.notification import send_notification, create_notification
+from octobot_trading.constants import CONFIG_TRADING_TENTACLES
 
 """Main OctoBot class:
 - Create all indicators and thread for each cryptocurrencies in config """
@@ -41,8 +43,8 @@ class OctoBot:
         self.start_time = time.time()
         self.config = config
         self.reset_trading_history = reset_trading_history
-        self.startup_config = copy.deepcopy(config)
-        self.edited_config = copy.deepcopy(config)
+        self.startup_config = copy.deepcopy(self.config)
+        self.edited_config = copy.deepcopy(self.config)
 
         # Used to know when OctoBot is ready to answer in APIs
         self.initialized = False
@@ -80,7 +82,18 @@ class OctoBot:
         await self.evaluator_factory.create()
         await self.interface_factory.create()
         await self.interface_factory.start_interfaces()
+        await self._post_initialize()
+
+    async def _post_initialize(self):
         self.initialized = True
+
+        # update startup_config and edited_config now that config contains all necessary info
+        # (tentacles config added in initialize)
+        # this might be temporary waiting for tentacle manager refactor
+        for config_element in (CONFIG_EVALUATOR, CONFIG_TRADING_TENTACLES):
+            self.startup_config[config_element] = copy.deepcopy(self.config[config_element])
+            self.edited_config[config_element] = copy.deepcopy(self.config[config_element])
+
         await send_notification(create_notification(f"{PROJECT_NAME} {LONG_VERSION} is starting ...",
                                                     markdown_format=MarkdownFormat.ITALIC))
 
