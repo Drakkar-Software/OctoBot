e236a569da0a31fc571da278eeec4b404a828c9d
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-01-12 23:08:46 +0100

    extract service feeds in dedicated file

--- octobot/evaluator_factory.py
+++ octobot/evaluator_factory.py
@@ -16,7 +16,6 @@
 
 from octobot_commons.logging.logging_util import get_logger
 from octobot_evaluators.api import create_matrix_channels, create_all_type_evaluators, initialize_evaluators
-from octobot_services.api.service_feeds import create_service_feed_factory, start_service_feed
 from octobot_trading.exchanges.exchanges import Exchanges
 from tools.logger import init_evaluator_chan_logger
 
@@ -43,7 +42,6 @@ class EvaluatorFactory:
     async def initialize(self):
         await initialize_evaluators(self.octobot.config)
         await create_matrix_channels()
-        self._initialize_service_feeds()
 
     async def create(self):
         for exchange_configuration in Exchanges.instance().get_all_exchanges():
@@ -53,19 +51,6 @@ class EvaluatorFactory:
                                              exchange_configuration.symbols,
                                              exchange_configuration.time_frames)
         await init_evaluator_chan_logger()
-        # Start service feeds now that evaluators registered their feed requirements
-        await self._start_service_feeds()
-
-    def _initialize_service_feeds(self):
-        service_feed_factory = create_service_feed_factory(self.octobot.config, self.octobot.async_loop)
-        self.service_feed_list = [service_feed_factory.create_service_feed(feed)
-                                  for feed in service_feed_factory.get_available_service_feeds()]
-
-    async def _start_service_feeds(self):
-        for feed in self.service_feed_list:
-            if not await start_service_feed(feed, False):
-                self.logger.error(f"Failed to start {feed.get_name()}. Evaluators requiring this service feed "
-                                  f"might not work properly")
 
     # def _check_required_evaluators(self):
     #     if self.symbol_tasks_manager:
---
+++ octobot/service_feed_factory.py
@@ -0,0 +1,42 @@
+#  Drakkar-Software OctoBot
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+from octobot_commons.logging.logging_util import get_logger
+from octobot_services.api.service_feeds import create_service_feed_factory, start_service_feed
+
+
+class ServiceFeedFactory:
+    """EvaluatorFactory class:
+    - Create service feeds
+    """
+
+    def __init__(self, octobot):
+        self.octobot = octobot
+
+        # Logger
+        self.logger = get_logger(self.__class__.__name__)
+
+        self.service_feeds = []
+
+    async def initialize(self):
+        service_feed_factory = create_service_feed_factory(self.octobot.config, self.octobot.async_loop)
+        self.service_feeds = [service_feed_factory.create_service_feed(feed)
+                              for feed in service_feed_factory.get_available_service_feeds()]
+
+    async def create(self):
+        for feed in self.service_feeds:
+            if not await start_service_feed(feed, False):
+                self.logger.error(f"Failed to start {feed.get_name()}. Evaluators requiring this service feed "
+                                  f"might not work properly")
