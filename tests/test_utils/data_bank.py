5e5a9ec890abf39be743e0beb3f0fa680e5e4af7
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-04-05 18:54:28 +0200

    update TA test framework for 0.4

--- tests/test_utils/data_bank.py
+++ tests/test_utils/data_bank.py
@@ -13,12 +13,12 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+from copy import deepcopy
 
-import ccxt
-import numpy as np
-
-from config import TimeFrames, PriceIndexes
-from octobot_trading.exchanges import ExchangeManager
+from octobot_backtesting.importers.exchanges.exchange_importer import ExchangeDataImporter
+from octobot_commons.constants import TimeFrames
+from octobot_commons.enums import PriceIndexes
+from octobot_trading.api.symbol_data import create_new_candles_manager
 from octobot_trading.util.initializable import Initializable
 
 """
@@ -37,98 +37,122 @@ TimeFrames.ONE_DAY: 2017-08-17 00:00:00 -> 2017-11-12 00:00:00
 
 class DataBank(Initializable):
 
-    def __init__(self, config, data_file=None, symbols=None):
+    def __init__(self, data_file=None):
         super().__init__()
-        self.config = config
-        self.data_file = data_file if data_file else "tests/static/binance_BTC_USDT_20180428_121156.data"
-        self.symbols = symbols if symbols else ["BTC"]
+        self.data_file = data_file if data_file else "tests/static/AbstractExchangeHistoryCollector_1586017993.616272.data"
+        self.exchange_name = None
+        self.data_importer = ExchangeDataImporter({}, self.data_file)
+        self.default_symbol = "BTC/USDT"
 
-        self.exchange_manager = ExchangeManager(self.config, ccxt.binance, is_simulated=True)
-        self.exchange_simulator_inst = None
-
+        self.origin_ohlcv_by_symbol = {}
+        self.candles_managers_by_time_frame = {}
+        self.current_init_indexes_by_time_frame = {}
+        self.current_data = None
         self.data_by_symbol_by_data_frame = None
 
     async def initialize_impl(self):
-        await self.exchange_manager.initialize()
-        self.exchange_simulator_inst = self.exchange_manager.get_exchange().get_exchange()
-        self._init_data()
+        await self.data_importer.initialize()
+        self.exchange_name = self.data_importer.exchange_name
+        for time_frame in self.data_importer.time_frames:
+            for symbol in self.data_importer.symbols:
+                if symbol not in self.origin_ohlcv_by_symbol:
+                    self.origin_ohlcv_by_symbol[symbol] = {}
+                db_data = await self.data_importer.get_ohlcv_from_timestamps(
+                    exchange_name=self.exchange_name,
+                    symbol=symbol,
+                    time_frame=time_frame
+                )
+                # store ohlcv only
+                self.origin_ohlcv_by_symbol[symbol][time_frame] = sorted(
+                    [data[-1] for data in db_data],
+                    key=lambda x: x[PriceIndexes.IND_PRICE_TIME.value]
+                )
 
-    def get_all_data_for_all_available_time_frames_for_symbol(self, symbol):
-        return self.data_by_symbol_by_data_frame[symbol]
+    async def stop(self):
+        await self.data_importer.stop()
+
+    def get_time_frames(self):
+        return self.data_importer.time_frames
+
+    def get_last_candle_for_default_symbol(self, time_frame):
+        return self.candles_managers_by_time_frame[time_frame].get_symbol_prices(1)
+
+    def set_data_for_default_symbol(self, time_frame, end_index):
+        self._set_candle_manager(time_frame, end_index, use_current_data=True)
+
+    def increment_data_for_default_symbol(self, time_frame):
+        current_index = self.current_init_indexes_by_time_frame[time_frame]
+        candle = self.origin_ohlcv_by_symbol[self.default_symbol][time_frame][current_index]
+        self.candles_managers_by_time_frame[time_frame].add_new_candle(candle)
+        self.current_init_indexes_by_time_frame[time_frame] += 1
+
+    # use default data with full data range
+    def standard_mode(self, initial_candles):
+        for time_frame in self.get_time_frames():
+            self._set_candle_manager(time_frame, initial_candles)
 
     # works only with default data file
     # not started, selling started, selling maxed, buying starting, max: back normal:
-    def get_rise_after_over_sold(self):
-        return np.concatenate((self._get_bank_time_frame_data(TimeFrames.FOUR_HOURS, 35, 61),
-                              self._get_bank_time_frame_data(TimeFrames.FOUR_HOURS, 0, 49)), axis=1), \
-            35, 43, 46, 47
+    def rise_after_over_sold_mode(self):
+        self.current_data = self._merge_candles(
+            self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.FOUR_HOURS][35:61],
+            self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.FOUR_HOURS][0:49]
+        )
+        return TimeFrames.FOUR_HOURS, 35, 43, 46, 47, 75
 
     # works only with default data file
     # not started, buying started, buying maxed, start dipping, super dip, max: back normal:
-    def get_dip_after_over_bought(self):
-        return self._get_bank_time_frame_data(TimeFrames.ONE_DAY, 0, 90), \
-            -18, -14, -10, -9, -2
+    def dip_after_over_bought_mode(self):
+        self.current_data = self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.ONE_DAY]
+        return TimeFrames.ONE_DAY, 72, 76, 80, 81, 88, 90
 
     # works only with default data file
     # not started, started, heavy dump, very light dump, max: stopped dump:
-    def get_sudden_dump(self):
-        return self._get_bank_time_frame_data(TimeFrames.ONE_HOUR, 0, 46), \
-            -4, -3, -2, -1
+    def sudden_dump_mode(self):
+        self.current_data = self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.ONE_HOUR]
+        return TimeFrames.ONE_HOUR, 42, 43, 44, 45, 46
 
     # works only with default data file
     # not started, started, heavy pump, max pump, change trend, dipping, max: dipped:
-    def get_sudden_pump(self):
-        return self._get_bank_time_frame_data(TimeFrames.HEIGHT_HOURS, 0, 83), \
-            71, 74, 76, 77, 78, 79
+    def sudden_pump_mode(self):
+        self.current_data = self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.HEIGHT_HOURS]
+        return TimeFrames.HEIGHT_HOURS, 71, 74, 76, 77, 78, 79, 83
 
     # works only with default data file
     # long data_frame with flat then sudden big rise and then mostly flat for 120 values
     # start move ending up in a rise, reaches flat trend, first micro up p1, first mirco up p2, micro down, micro up,
     # micro down, micro up, micro down, back normal, micro down, back normal, micro down, back up, micro up, back down,
     # back normal, micro down, back up, micro down, back up, micro down, back up
-    def get_overall_flat_trend(self):
-        return np.concatenate((self._get_bank_time_frame_data(TimeFrames.FIFTEEN_MINUTES, 6, 72)*0.9,
-                              self._get_bank_time_frame_data(TimeFrames.HEIGHT_HOURS, 25, 43),
-                              self._get_bank_time_frame_data(TimeFrames.FIFTEEN_MINUTES, 6, 72),
-                              self._get_bank_time_frame_data(TimeFrames.FIFTEEN_MINUTES, 6, 72)), axis=1), \
-            67, 85, 87, 90, 95, 107, 116, 123, 138, 141, 151, 153, 161, 163, 173, 174, 175, 180, 183, 193, 198, 203, 207
+    def overall_flat_trend_mode(self):
+        cheap_prices = [
+            [data * 0.9 for data in candle]
+            for candle in self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.FIFTEEN_MINUTES][6:72]
+        ]
+        self.current_data = self._merge_candles(
+            cheap_prices,
+            self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.HEIGHT_HOURS][25:43],
+            self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.FIFTEEN_MINUTES][6:72],
+            self.origin_ohlcv_by_symbol[self.default_symbol][TimeFrames.FIFTEEN_MINUTES][6:72]
+        )
+        return TimeFrames.FIFTEEN_MINUTES, 67, 85, 87, 90, 95, 107, 116, 123, 138, 141, 151, 153, 161, 163, 173, \
+            174, 175, 180, 183, 193, 198, 203, 207
 
-    def _get_bank_time_frame_data(self, time_frame, min_index=None, max_index=None):
-        if max_index is None and min_index is None:
-            return self.data_by_symbol_by_data_frame[self.symbols[0]][time_frame]
-        else:
-            min_i = min_index or 0
-            max_i = max_index or len(self.data_by_symbol_by_data_frame[self.symbols[0]][time_frame][0])-1
-            return self.reduce_data(self.data_by_symbol_by_data_frame[self.symbols[0]][time_frame], min_i, max_i)
-
-    def _init_data(self):
-        self.data_by_symbol_by_data_frame = {symbol: self._get_symbol_data(symbol) for symbol in self.symbols}
-
-    def _get_symbol_data(self, symbol):
-        min_index = 0
-        max_index = min_index + 100
-        return {time_frame: self.exchange_simulator_inst.get_candles_exact(
-            symbol,
-            time_frame,
-            min_index,
-            max_index,
-            return_list=False)
-            for time_frame in TimeFrames
-            if self.exchange_simulator_inst.has_data_for_time_frame(symbol, time_frame.value)}
+    def _set_candle_manager(self, time_frame, initial_candles_max_index, use_current_data=False):
+        self.candles_managers_by_time_frame[time_frame] = create_new_candles_manager(
+            self.current_data[:initial_candles_max_index] if use_current_data else
+            self.origin_ohlcv_by_symbol[self.default_symbol][time_frame][:initial_candles_max_index]
+        )
+        self.current_init_indexes_by_time_frame[time_frame] = initial_candles_max_index
 
     @staticmethod
-    def reduce_data(time_frame_data, min_index, max_index):
-        symbol_prices = [None]*len(PriceIndexes)
-        symbol_prices[PriceIndexes.IND_PRICE_CLOSE.value] = \
-            time_frame_data[PriceIndexes.IND_PRICE_CLOSE.value][min_index:max_index]
-        symbol_prices[PriceIndexes.IND_PRICE_OPEN.value] = \
-            time_frame_data[PriceIndexes.IND_PRICE_OPEN.value][min_index:max_index]
-        symbol_prices[PriceIndexes.IND_PRICE_HIGH.value] = \
-            time_frame_data[PriceIndexes.IND_PRICE_HIGH.value][min_index:max_index]
-        symbol_prices[PriceIndexes.IND_PRICE_LOW.value] = \
-            time_frame_data[PriceIndexes.IND_PRICE_LOW.value][min_index:max_index]
-        symbol_prices[PriceIndexes.IND_PRICE_TIME.value] = \
-            time_frame_data[PriceIndexes.IND_PRICE_TIME.value][min_index:max_index]
-        symbol_prices[PriceIndexes.IND_PRICE_VOL.value] = \
-            time_frame_data[PriceIndexes.IND_PRICE_VOL.value][min_index:max_index]
-        return np.array(symbol_prices)
+    def _merge_candles(*candles_list):
+        result_list = deepcopy(candles_list[0])
+        timestamp_list = [candle[PriceIndexes.IND_PRICE_TIME.value] for candle in result_list]
+        for candle_list in candles_list[1:]:
+            # ensure no timestamp is present in 2 candles
+            while any(candle[PriceIndexes.IND_PRICE_TIME.value] in timestamp_list for candle in candle_list):
+                for candle in candle_list:
+                    candle[PriceIndexes.IND_PRICE_TIME.value] = candle[PriceIndexes.IND_PRICE_TIME.value] + 1
+            result_list += deepcopy(candle_list)
+            timestamp_list = [candle[PriceIndexes.IND_PRICE_TIME.value] for candle in result_list]
+        return result_list
