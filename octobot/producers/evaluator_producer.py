bb36feb88f8c99a7c69d0c42d8772b9d808ae836
herklos <herklos.dev@protonmail.com>
2020-05-16 13:02:22 +0200

    [OctoBotChannel] Add evaluator consumer

--- octobot/producers/evaluator_producer.py
+++ octobot/producers/evaluator_producer.py
@@ -16,9 +16,11 @@
 from octobot.channels.octobot_channel import OctoBotChannelProducer
 from octobot.logger import init_evaluator_chan_logger
 from octobot_backtesting.api.backtesting import is_backtesting_enabled
-from octobot_evaluators.api.evaluators import initialize_evaluators, create_all_type_evaluators
+from octobot_commons.enums import OctoBotChannelSubjects
+from octobot_evaluators.api.evaluators import initialize_evaluators
 from octobot_evaluators.api.initialization import create_evaluator_channels
-from octobot_trading.exchanges.exchanges import Exchanges
+from octobot_evaluators.consumers.octobot_channel_consumer import OctoBotChannelEvaluatorActions as EvaluatorActions, \
+    OctoBotChannelEvaluatorDataKeys as EvaluatorKeys
 
 
 class EvaluatorProducer(OctoBotChannelProducer):
@@ -36,18 +38,17 @@ class EvaluatorProducer(OctoBotChannelProducer):
     async def start(self):
         self.matrix_id = await initialize_evaluators(self.octobot.config, self.tentacles_setup_config)
         await create_evaluator_channels(self.matrix_id, is_backtesting=is_backtesting_enabled(self.octobot.config))
-
-    async def create_evaluator(self):
-        for exchange_configuration in Exchanges.instance().get_all_exchanges():
-            await create_all_type_evaluators(
-                self.octobot.config,
-                tentacles_setup_config=self.tentacles_setup_config,
-                matrix_id=self.matrix_id,
-                exchange_name=exchange_configuration.exchange_name,
-                bot_id=self.octobot.bot_id,
-                symbols_by_crypto_currencies=exchange_configuration.symbols_by_crypto_currencies,
-                symbols=exchange_configuration.symbols,
-                time_frames=exchange_configuration.time_frames_without_real_time,
-                real_time_time_frames=exchange_configuration.real_time_time_frames
-            )
         await init_evaluator_chan_logger(self.matrix_id)
+
+    async def create_evaluators(self, exchange_configuration):
+        await self.send(bot_id=self.octobot.bot_id,
+                        subject=OctoBotChannelSubjects.CREATION.value,
+                        action=EvaluatorActions.EVALUATOR.value,
+                        data={
+                            EvaluatorKeys.TENTACLES_SETUP_CONFIG.value: self.octobot.tentacles_setup_config,
+                            EvaluatorKeys.MATRIX_ID.value: self.octobot.evaluator_producer.matrix_id,
+                            EvaluatorKeys.EXCHANGE_CONFIGURATION.value: exchange_configuration,
+                            EvaluatorKeys.CONFIG.value: self.octobot.config
+                        })
+
+
