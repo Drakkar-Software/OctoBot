4be482dded06299abb735c712f9233e387c9d1cb
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-10-31 22:28:46 +0100

    add community authentication handler

--- octobot/octobot_api.py
+++ octobot/octobot_api.py
@@ -15,6 +15,7 @@
 #  License along with this library.
 import octobot.constants as constants
 import octobot.commands as commands
+import octobot.community
 
 
 class OctoBotAPI:
@@ -57,6 +58,9 @@ class OctoBotAPI:
 
     def get_aiohttp_session(self) -> object:
         return self._octobot.get_aiohttp_session()
+
+    def get_community_auth(self) -> octobot.community.CommunityAuthentication:
+        return self._octobot.community_auth
 
     def run_in_main_asyncio_loop(self, coroutine):
         return self._octobot.run_in_main_asyncio_loop(coroutine)
