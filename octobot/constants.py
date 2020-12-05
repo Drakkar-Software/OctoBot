b42ac2e9e1a1168fa2312e5c81c750673c070ba2
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-12-05 15:33:52 +0100

    add octobot wiki in constants

--- octobot/constants.py
+++ octobot/constants.py
@@ -27,6 +27,7 @@ VERSION = f"{SHORT_VERSION}{VERSION_DEV_PHASE}{VERSION_PHASE}"
 LONG_VERSION = f"{SHORT_VERSION}{PATCH_VERSION}{VERSION_DEV_PHASE}{VERSION_PHASE}"
 
 # OctoBot urls
+OCTOBOT_WIKI_URL = "https://github.com/Drakkar-Software/OctoBot/wiki"
 OCTOBOT_ONLINE = "https://www.tentacles.octobot.online"
 REPOSITORY = "repository"
 TENTACLES_REPOSITORY = "tentacles"
