c6fadbcc9e0d1da74a32fa069e2a8c762dd0e8d4
Herklos <herklos.dev@protonmail.com>
2021-01-09 23:34:01 +0100

    [CI] Add dev branch in docker ci

--- tests/docker-entrypoint.sh
+++ tests/docker-entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # install tentacles
-./OctoBot tentacles --install -a
+if ./OctoBot tentacles --install -a ; then
+    echo "Tentacles successfully installed"
+else
+    export TENTACLES_PACKAGES_SOURCE=officials
+    export TENTACLES_URL_TAG=latest
+    ./OctoBot tentacles --install -a
+fi
 
 # run tests
 pytest -rw tests tentacles
