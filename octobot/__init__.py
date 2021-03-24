4733355f2879ff2f9e179253108370891fc94fab
herklos <herklos.dev@protonmail.com>
2021-03-24 23:53:58 +0100

    [Install] Fix exception when installing OctoBot from pip or setup.py

--- octobot/__init__.py
+++ octobot/__init__.py
@@ -15,29 +15,9 @@
 #  License along with this library.
 
 from __future__ import print_function
-# prevents distutils_patch.py:26: UserWarning: Distutils was imported before Setuptools. This usage is discouraged
-# and may exhibit undesirable behaviors or errors. Please use Setuptools' objects directly or at least import Setuptools
-# first
-import setuptools
-from distutils.version import LooseVersion
 import os
 import sys
 
-MIN_TENTACLE_MANAGER_VERSION = "1.0.10"
-
-# check compatible tentacle manager
-try:
-    from octobot_tentacles_manager import VERSION
-
-    if LooseVersion(VERSION) < MIN_TENTACLE_MANAGER_VERSION:
-        print("OctoBot requires OctoBot-Tentacles-Manager in a minimum version of " + MIN_TENTACLE_MANAGER_VERSION +
-              " you can install and update OctoBot-Tentacles-Manager using the following command: "
-              "python3 -m pip install -U OctoBot-Tentacles-Manager", file=sys.stderr)
-        sys.exit(-1)
-except ImportError:
-    print("OctoBot requires OctoBot-Tentacles-Manager, you can install it using "
-          "python3 -m pip install -U OctoBot-Tentacles-Manager", file=sys.stderr)
-    sys.exit(-1)
 
 # binary tentacle importation
 sys.path.append(os.path.dirname(sys.executable))
@@ -66,21 +46,3 @@ def get_bot():
 
 def get_config():
     return global_config
-
-
-from octobot import octobot
-
-from octobot.octobot import (
-    OctoBot
-)
-
-from octobot import octobot_backtesting_factory
-
-from octobot.octobot_backtesting_factory import (
-    OctoBotBacktestingFactory,
-)
-
-__all__ = [
-    "OctoBot",
-    "OctoBotBacktestingFactory",
-]
