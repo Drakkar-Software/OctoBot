82058cc7fa9d61503b00ca6275542bdb67a93bd8
Herklos <herklos.dev@protonmail.com>
2021-04-08 17:12:52 +0200

    [Tests] Add basic updater unit tests

--- tests/unit_tests/updater/test_updater.py
+++ tests/unit_tests/updater/test_updater.py
@@ -13,3 +13,18 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+import pytest
+
+import octobot.api as octobot_api
+
+# All test coroutines will be treated as marked.
+pytestmark = pytest.mark.asyncio
+
+
+async def test_create_updater_from_api():
+    updater = octobot_api.get_updater()
+
+
+async def test_should_update_on_updater_from_api():
+    updater = octobot_api.get_updater()
+    assert not (await updater.should_be_updated())
