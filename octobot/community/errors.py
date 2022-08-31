a79111afcb8ef3ca6ed397e9fcd9086d565c1c16
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-08-31 23:58:58 +0000

    [Feeds] add DeviceError

--- octobot/community/errors.py
+++ octobot/community/errors.py
@@ -13,6 +13,7 @@
 #
 #  You should have received a copy of the GNU General Public
 #  License along with OctoBot. If not, see <https://www.gnu.org/licenses/>.
+import octobot_commons.authentication as commons_authentication
 
 
 class RequestError(Exception):
@@ -20,4 +21,8 @@ class RequestError(Exception):
 
 
 class StatusCodeRequestError(RequestError):
+    pass
+
+
+class DeviceError(commons_authentication.UnavailableError):
     pass
