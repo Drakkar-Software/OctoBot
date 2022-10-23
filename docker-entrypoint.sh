f4631d6a116351fdb50571410f97e754c8b9a914
Herklos <herklos.dev@protonmail.com>
2022-10-23 13:19:57 +0000

    [Docker] Add cloudflared to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# OctoBot user config from env
 if [[ -n "${OCTOBOT_CONFIG}" ]]; then
   echo "$OCTOBOT_CONFIG" | tee /octobot/user/config.json > /dev/null
+fi
+
+##### CLOUDFLARED #####
+export TUNNEL_LOGFILE=/root/cloudflared.log
+export TUNNEL_LOGLEVEL=info
+
+# start cloudflared if token is provided
+if [[ -n "$CLOUDFLARE_TOKEN" ]]; then
+  cloudflared tunnel --url http://localhost:5001 --no-autoupdate run --token $CLOUDFLARE_TOKEN &
 fi
 
 ./OctoBot
