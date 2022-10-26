9added8d0f238d54e8015132298f2f1956cb9712
Herklos <herklos.dev@protonmail.com>
2022-10-26 21:05:44 +0000

    [Docker] Migrate aws to python

---
+++ docker/aws.py
@@ -0,0 +1,24 @@
+import os
+import json
+import re
+from urllib.request import urlopen
+
+
+def remove_special_chars(input_string):
+    return re.sub(r'[^a-zA-Z0-9_]', '', input_string)
+
+
+if __name__ == '__main__':
+    # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html
+    container_metadata_url = os.getenv("ECS_CONTAINER_METADATA_URI_V4", None)
+    if container_metadata_url is not None:
+        try:
+            with urlopen(container_metadata_url + "/taskWithTags") as response:
+                body = response.read()
+            container_metadata = json.loads(body)
+            with open('.env', 'w') as env_file:
+                for key, value in container_metadata['TaskTags'].items():
+                    env_file.write(remove_special_chars(
+                        key) + "=" + value+"\n")
+        except Exception as e:
+            print("Error when requesting or parsing aws metadata")
