597ae0aa5ae495a655f8cb7ce76656c5e6b4b261
Herklos <herklos.dev@protonmail.com>
2022-10-31 22:52:49 +0000

    [DotEnv] Add DOTENV_PATH

--- docker/aws.py
+++ docker/aws.py
@@ -11,12 +11,13 @@ def remove_special_chars(input_string):
 if __name__ == '__main__':
     # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html
     container_metadata_url = os.getenv("ECS_CONTAINER_METADATA_URI_V4", None)
+    dotenv_path = os.getenv("DOTENV_PATH", os.path.curdir)
     if container_metadata_url is not None:
         try:
             with urlopen(container_metadata_url + "/taskWithTags") as response:
                 body = response.read()
             container_metadata = json.loads(body)
-            with open('.env', 'w') as env_file:
+            with open(os.path.join(dotenv_path, ".env"), 'w') as env_file:
                 for key, value in container_metadata['TaskTags'].items():
                     env_file.write(remove_special_chars(
                         key) + "=" + value+"\n")
