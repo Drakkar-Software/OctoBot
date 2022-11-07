a43680324bc9f246ebb36162bd27ca7d6f09bead
Herklos <herklos.dev@protonmail.com>
2022-11-07 21:44:16 +0000

    [Docker] Add s3fs support

---
+++ docker/s3fs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export S3FS_MOUNT_POINT=/octobot/user
+
+# load env file
+ENV_FILE=.env
+if [[ -f "$ENV_FILE" ]]; then
+    source $ENV_FILE
+fi
+
+# start s3fs if bucket name is provided
+# https://github.com/s3fs-fuse/s3fs-fuse
+if [[ -n "$S3FS_BUCKET_NAME" ]]; then
+   s3fs $S3FS_BUCKET_NAME $S3FS_MOUNT_POINT -o iam_role=auto -o allow_other -o umask=000
+fi
--- docker/tunnel.sh
+++ docker/tunnel.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-source .env
-
 export TUNNEL_LOGFILE=/root/cloudflared.log
 export TUNNEL_LOGLEVEL=info
+
+# load env file
+ENV_FILE=.env
+if [[ -f "$ENV_FILE" ]]; then
+    source $ENV_FILE
+fi
 
 # start cloudflared if token is provided
 # https://developers.cloudflare.com/cloudflare-one/tutorials/cli/
