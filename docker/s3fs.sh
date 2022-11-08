33d3120be528ac2a3f635eb5712c9b6e31484d2c
Herklos <herklos.dev@protonmail.com>
2022-11-08 22:27:26 +0000

    f

--- docker/s3fs.sh
+++ docker/s3fs.sh
@@ -11,5 +11,5 @@ fi
 # start s3fs if bucket name is provided
 # https://github.com/s3fs-fuse/s3fs-fuse
 if [[ -n "$S3FS_BUCKET_NAME" ]]; then
-   s3fs $S3FS_BUCKET_NAME $S3FS_MOUNT_POINT -o iam_role=auto -o allow_other -o umask=000
+   s3fs $S3FS_BUCKET_NAME $S3FS_MOUNT_POINT -o use_cache=/tmp -o iam_role=auto -o allow_other -o umask=000
 fi
