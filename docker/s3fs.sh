0a7774e5eec5acb38de81da67482996ede50ac4b
Herklos <herklos.dev@protonmail.com>
2022-11-11 09:01:05 +0000

    [Docker] Fix bucket region

--- docker/s3fs.sh
+++ docker/s3fs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export S3FS_MOUNT_POINT=/octobot/user
+export S3FS_CACHE_DIR=/tmp/s3fs
 
 # load env file
 ENV_FILE=.env
@@ -11,9 +12,7 @@ fi
 # start s3fs if bucket name is provided
 # https://github.com/s3fs-fuse/s3fs-fuse
 # https://www.mankier.com/1/s3fs#Performance_Considerations-Performance_of_S3_requests
-# TODO : 
-# - fix -o use_cache=/tmp
-# - try -o ecs
 if [[ -n "$S3FS_BUCKET_NAME" ]]; then
-   s3fs $S3FS_BUCKET_NAME $S3FS_MOUNT_POINT -o iam_role=auto -o allow_other -o umask=000 -o enable_noobj_cache -o notsup_compat_dir -o multireq_max=50
+    mkdir -p $S3FS_CACHE_DIR
+    s3fs $S3FS_BUCKET_NAME $S3FS_MOUNT_POINT -o endpoint=$S3FS_BUCKET_REGION -o iam_role=auto -o allow_other -o umask=000 -o enable_noobj_cache -o notsup_compat_dir -o multireq_max=50 -o use_cache=$S3FS_CACHE_DIR
 fi
