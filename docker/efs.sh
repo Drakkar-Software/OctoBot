fb5647ee24753dad238133b3b47ad60ca766356d
Herklos <herklos.dev@protonmail.com>
2022-11-12 19:55:36 +0000

    [Docker][EFS] Update to user mount point

--- docker/efs.sh
+++ docker/efs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export EFS_MOUNT_POINT=/octobot/user/
+export EFS_USER_MOUNT_POINT=/octobot/user/
 
 # load env file
 ENV_FILE=.env
@@ -11,5 +11,5 @@ fi
 # start efs if file system id is provided
 # https://github.com/aws/efs-utils
 if [[ -n "$EFS_FS_ENDPOINT" ]]; then
-    mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $EFS_FS_ENDPOINT:/ $EFS_MOUNT_POINT
+    mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $EFS_FS_ENDPOINT:/user/ $EFS_USER_MOUNT_POINT
 fi
