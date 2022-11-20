5b79752bd3283382de87db07370da8a271be2559
Herklos <herklos.dev@protonmail.com>
2022-11-20 10:08:07 +0000

    [Docker] Improve error handling in entrypoint

--- docker/efs.sh
+++ docker/efs.sh
@@ -10,6 +10,7 @@ fi
 
 # start efs if file system id is provided
 # https://github.com/aws/efs-utils
+# 200 = 20sec timeout
 if [[ -n "$EFS_FS_ENDPOINT" ]]; then
-    mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $EFS_FS_ENDPOINT:/ $EFS_USER_MOUNT_POINT
+    mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=200,retrans=2,noresvport $EFS_FS_ENDPOINT:/ $EFS_USER_MOUNT_POINT
 fi
