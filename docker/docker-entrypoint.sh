22de01bdcc95551b526f3c327b07aa2a2288302b
Herklos <herklos.dev@protonmail.com>
2022-11-20 10:08:07 +0000

    [Docker] Improve error handling in entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status 
+set -e
+
 # Save OctoBot config
 if [[ -n "${OCTOBOT_CONFIG}" ]]; then
   echo "$OCTOBOT_CONFIG" | tee /octobot/user/config.json >/dev/null
@@ -16,6 +19,9 @@ bash efs.sh
 
 # Start cloudflared tunnel
 bash tunnel.sh
+
+# Disable set -e 
+set +e
 
 # Start OctoBot
 ./OctoBot
