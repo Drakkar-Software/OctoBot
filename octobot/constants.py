56080ef9d4096e624700d2500c0b825e713e8d17
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-11-23 23:36:30 +0100

    [Version] v0.4.22

--- octobot/constants.py
+++ octobot/constants.py
@@ -23,7 +23,7 @@ import octobot.enums
 
 PROJECT_NAME = "OctoBot"
 AUTHOR = "DrakkarSoftware"
-SHORT_VERSION = "0.4.21"  # major.minor.revision
+SHORT_VERSION = "0.4.22"  # major.minor.revision
 PATCH_VERSION = ""  # patch : pX
 VERSION_DEV_PHASE = ""  # alpha : a / beta : b / release candidate : rc
 VERSION_PHASE = ""  # XX
