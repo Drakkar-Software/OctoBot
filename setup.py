dd47185055d0ee5eb49be10a2e5b7b21f0b8d50b
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-05-02 19:25:39 +0200

    [Cython] remove

--- setup.py
+++ setup.py
@@ -13,59 +13,11 @@
 #
 #  You should have received a copy of the GNU General Public
 #  License along with OctoBot. If not, see <https://www.gnu.org/licenses/>.
-import os
-from setuptools import dist
-
-dist.Distribution().fetch_build_eggs(['Cython==0.29.32'])
-
-try:
-    from Cython.Distutils import build_ext
-    from Cython.Build import cythonize
-except ImportError:
-    # create closure for deferred import
-    def cythonize(*args, **kwargs):
-        from Cython.Build import cythonize
-        return cythonize(*args, **kwargs)
-
-
-    def build_ext(*args, **kwargs):
-        from Cython.Distutils import build_ext
-        return build_ext(*args, **kwargs)
-
 from setuptools import find_packages
-from setuptools import setup, Extension
+from setuptools import setup
 from octobot import PROJECT_NAME, AUTHOR, VERSION
 
 PACKAGES = find_packages(exclude=["tentacles*", "tests", ])
-
-packages_list = [
-    "octobot.configuration_manager",
-    "octobot.octobot_backtesting_factory",
-    "octobot.octobot_api",
-    "octobot.task_manager",
-    "octobot.octobot_channel_consumer",
-    "octobot.initializer",
-    "octobot.octobot",
-    "octobot.backtesting.independent_backtesting",
-    "octobot.backtesting.abstract_backtesting_test",
-    "octobot.backtesting.octobot_backtesting",
-    "octobot.channels.octobot_channel",
-    "octobot.strategy_optimizer.strategy_test_suite",
-    "octobot.strategy_optimizer.test_suite_result",
-    "octobot.strategy_optimizer.strategy_optimizer",
-    "octobot.producers.interface_producer",
-    "octobot.producers.service_feed_producer",
-    "octobot.producers.evaluator_producer",
-    "octobot.producers.exchange_producer",
-    "octobot.updater.binary_updater",
-    "octobot.updater.python_updater",
-    "octobot.updater.updater",
-    "octobot.updater.updater_factory",
-]
-
-ext_modules = [
-    Extension(package, [f"{package.replace('.', '/')}.py"])
-    for package in packages_list]
 
 # long description from README file
 with open('README.md', encoding='utf-8') as f:
@@ -78,7 +30,6 @@ def ignore_git_requirements(requirements):
 
 REQUIRED = ignore_git_requirements(open('requirements.txt').readlines())
 REQUIRES_PYTHON = '>=3.8'
-CYTHON_DEBUG = False if not os.getenv('CYTHON_DEBUG') else os.getenv('CYTHON_DEBUG')
 
 setup(
     name=PROJECT_NAME,
@@ -86,7 +37,7 @@ setup(
     url='https://github.com/Drakkar-Software/OctoBot',
     license='GPL-3.0',
     author=AUTHOR,
-    author_email='drakkar-software@protonmail.com',
+    author_email='contact@drakkar.software',
     description='Cryptocurrencies alert / trading bot',
     py_modules=['start'],
     packages=PACKAGES,
@@ -95,13 +46,10 @@ setup(
     },
     long_description=DESCRIPTION,
     long_description_content_type='text/markdown',
-    cmdclass={'build_ext': build_ext},
     tests_require=["pytest"],
     test_suite="tests",
     zip_safe=False,
-    setup_requires=REQUIRED if not CYTHON_DEBUG else [],
     install_requires=REQUIRED,
-    ext_modules=cythonize(ext_modules, gdb_debug=CYTHON_DEBUG),
     python_requires=REQUIRES_PYTHON,
     entry_points={
         'console_scripts': [
@@ -115,6 +63,5 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Cython'
     ],
 )
