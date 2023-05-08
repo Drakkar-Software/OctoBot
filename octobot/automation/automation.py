aa959bd3cb0dea6a5a8f0122a8edb4eebde5813d
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-05-08 18:52:24 +0200

    [Automations] do not interrupt loading on missing tentacle

--- octobot/automation/automation.py
+++ octobot/automation/automation.py
@@ -193,7 +193,10 @@ class Automation(tentacles_management.AbstractTentacle):
 
     def _apply_user_inputs(self, step_names, step_classes_by_name: dict, inputs, automation_id):
         for step_name in step_names:
-            self._apply_step_user_inputs(step_name, step_classes_by_name[step_name], inputs, automation_id)
+            try:
+                self._apply_step_user_inputs(step_name, step_classes_by_name[step_name], inputs, automation_id)
+            except KeyError:
+                self.logger.exception(f"Automation step not found: {step_name} (ignored)")
 
     def _apply_step_user_inputs(self, step_name, step_class, inputs, automation_id):
         step = step_class()
