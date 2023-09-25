18382c1110a2a10aad286a57e29bdce473354540
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2023-09-25 20:23:55 +0200

    [Community] add landing links

--- octobot/community/identifiers_provider.py
+++ octobot/community/identifiers_provider.py
@@ -21,6 +21,7 @@ import octobot_commons.configuration as configuration
 
 class IdentifiersProvider:
     ENABLED_ENVIRONMENT: str = None
+    COMMUNITY_LANDING_URL: str = None
     COMMUNITY_URL: str = None
     FRONTEND_PASSWORD_RECOVER_URL: str = None
     BACKEND_URL: str = None
@@ -29,6 +30,7 @@ class IdentifiersProvider:
     @staticmethod
     def use_production():
         IdentifiersProvider.COMMUNITY_URL = constants.OCTOBOT_COMMUNITY_URL
+        IdentifiersProvider.COMMUNITY_LANDING_URL = constants.OCTOBOT_COMMUNITY_LANDING_URL
         IdentifiersProvider.FRONTEND_PASSWORD_RECOVER_URL = constants.OCTOBOT_COMMUNITY_RECOVER_PASSWORD_URL
         IdentifiersProvider.BACKEND_URL = constants.COMMUNITY_BACKEND_URL
         IdentifiersProvider.BACKEND_KEY = constants.COMMUNITY_BACKEND_KEY
@@ -37,6 +39,7 @@ class IdentifiersProvider:
     @staticmethod
     def use_staging():
         IdentifiersProvider.COMMUNITY_URL = constants.STAGING_OCTOBOT_COMMUNITY_URL
+        IdentifiersProvider.COMMUNITY_LANDING_URL = constants.STAGING_OCTOBOT_COMMUNITY_LANDING_URL
         IdentifiersProvider.FRONTEND_PASSWORD_RECOVER_URL = constants.STAGING_COMMUNITY_RECOVER_PASSWORD_URL
         IdentifiersProvider.BACKEND_URL = constants.STAGING_COMMUNITY_BACKEND_URL
         IdentifiersProvider.BACKEND_KEY = constants.STAGING_COMMUNITY_BACKEND_KEY
