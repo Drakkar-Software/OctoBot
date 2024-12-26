2ffc5868632849dd1ed4335c831a19b39ec620b9
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-12-26 12:26:47 +0100

    [Requirements] bump

--- additional_tests/exchanges_tests/test_kucoin_futures.py
+++ additional_tests/exchanges_tests/test_kucoin_futures.py
@@ -27,17 +27,18 @@ class TestKucoinFuturesAuthenticatedExchange(
     # enter exchange name as a class variable here
     EXCHANGE_NAME = "kucoin"
     CREDENTIALS_EXCHANGE_NAME = "KUCOIN_FUTURES"
-    ORDER_CURRENCY = "DOT"  # DOT to allow for smaller orders
+    ORDER_CURRENCY = "SOL"  # always use a contract that has a size different from 1 unit of the currency
     SETTLEMENT_CURRENCY = "USDT"
     SYMBOL = f"{ORDER_CURRENCY}/{SETTLEMENT_CURRENCY}:{SETTLEMENT_CURRENCY}"
     INVERSE_SYMBOL = f"{ORDER_CURRENCY}/USD:{ORDER_CURRENCY}"
-    ORDER_SIZE = 10  # % of portfolio to include in test orders
+    ORDER_SIZE = 5  # % of portfolio to include in test orders
     SUPPORTS_GET_LEVERAGE = False
     SUPPORTS_SET_LEVERAGE = False
     USE_ORDER_OPERATION_TO_CHECK_API_KEY_RIGHTS = True
     VALID_ORDER_ID = "6617e84c5c1e0000083c71f7"
     EXPECT_MISSING_FEE_IN_CANCELLED_ORDERS = False
     IS_AUTHENTICATED_REQUEST_CHECK_AVAILABLE = True    # set True when is_authenticated_request is implemented
+    EXPECTED_QUOTE_MIN_ORDER_SIZE = 40
 
     async def test_get_portfolio(self):
         await super().test_get_portfolio()
