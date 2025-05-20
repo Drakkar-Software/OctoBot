544f0c8116779a0e20556456a187aa25529ac915
Herklos <herklos.dev@protonmail.com>
2025-05-20 23:00:34 +0200

    [Docker] Remove efs lib

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,15 +8,6 @@ if [[ -n "${OCTOBOT_CONFIG}" ]]; then
   echo "$OCTOBOT_CONFIG" | tee /octobot/user/config.json >/dev/null
 fi
 
-# Trigger AWS env loading
-python aws.py
-
-# Mount s3fs
-bash s3fs.sh
-
-# Mount efs
-bash efs.sh
-
 # Start cloudflared tunnel
 bash tunnel.sh
 
