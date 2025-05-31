c591dbb2e6cc82ca35118d96ae1ba0a113137a87
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2025-05-31 23:15:49 +0200

    [Tests] add kucoin edit order test

--- additional_tests/exchanges_tests/test_kucoin.py
+++ additional_tests/exchanges_tests/test_kucoin.py
@@ -126,8 +126,7 @@ class TestKucoinAuthenticatedExchange(
         await super().test_create_and_cancel_stop_orders()
 
     async def test_edit_limit_order(self):
-        # pass if not implemented
-        pass
+        await super().test_edit_limit_order()
 
     async def test_edit_stop_order(self):
         # pass if not implemented
