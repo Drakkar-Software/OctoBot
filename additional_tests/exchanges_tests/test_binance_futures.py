6d91518a990f4f16d4f16c634e1f836823d276ff
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2025-06-01 18:25:32 +0200

    [Tests] add edit order tests

--- additional_tests/exchanges_tests/test_binance_futures.py
+++ additional_tests/exchanges_tests/test_binance_futures.py
@@ -169,12 +169,10 @@ class TestBinanceFuturesAuthenticatedExchange(
         await super().test_create_and_cancel_stop_orders()
 
     async def test_edit_limit_order(self):
-        # pass if not implemented
-        pass
+        await super().test_edit_limit_order()
 
     async def test_edit_stop_order(self):
-        # pass if not implemented
-        pass
+        await super().test_edit_stop_order()
 
     async def test_create_single_bundled_orders(self):
         # pass if not implemented
