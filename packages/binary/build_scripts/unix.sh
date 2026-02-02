a6ff95e34beae26db907f3cd291ce9ad72941378
Herklos <herklos@drakkar.software>
2026-02-02 18:10:14 +0100

    Update binary setup

--- packages/binary/build_scripts/unix.sh
+++ packages/binary/build_scripts/unix.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 python3 -m pip install -U pip setuptools wheel
-python3 -m pip install -r requirements.txt
-git clone -q $OCTOBOT_GH_REPO -b $OCTOBOT_DEFAULT_BRANCH
-python3 -m pip install --prefer-binary -r $OCTOBOT_REPOSITORY_DIR/dev_requirements.txt -r $OCTOBOT_REPOSITORY_DIR/requirements.txt -r $OCTOBOT_REPOSITORY_DIR/full_requirements.txt
+pants package :OctoBot
+pip3 install dist/octobot-*.whl
 python3 -m pip freeze
 python3 scripts/python_file_lister.py bin/octobot_packages_files.txt $OCTOBOT_REPOSITORY_DIR
 python3 scripts/insert_imports.py $OCTOBOT_REPOSITORY_DIR/octobot/cli.py
--- packages/binary/build_scripts/windows.ps1
+++ packages/binary/build_scripts/windows.ps1
@@ -1,7 +1,6 @@
 python -m pip install -U pip setuptools wheel
-python -m pip install -r requirements.txt
-git clone -q $env:OCTOBOT_GH_REPO -b $env:OCTOBOT_DEFAULT_BRANCH
-python -m pip install --prefer-binary -r $env:OCTOBOT_REPOSITORY_DIR/dev_requirements.txt -r $env:OCTOBOT_REPOSITORY_DIR/requirements.txt -r $env:OCTOBOT_REPOSITORY_DIR/full_requirements.txt
+pants package :OctoBot
+python -m pip install dist/octobot-*.whl
 python -m pip freeze
 python scripts/python_file_lister.py bin/octobot_packages_files.txt $env:OCTOBOT_REPOSITORY_DIR
 python scripts/insert_imports.py $env:OCTOBOT_REPOSITORY_DIR/octobot/cli.py
