b19c8095ed1a5a47312bcdc2a103b07c32049503
Herklos <herklos@drakkar.software>
2026-02-03 10:41:49 +0100

    Fix binary
    Signed-off-by: Herklos <herklos@drakkar.software>

--- packages/binary/build_scripts/windows.ps1
+++ packages/binary/build_scripts/windows.ps1
@@ -1,13 +1,8 @@
-python -m pip install -U pip setuptools wheel
-pants package :OctoBot
-python -m pip install dist/octobot-*.whl
 python -m pip freeze
-python scripts/python_file_lister.py bin/octobot_packages_files.txt $env:OCTOBOT_REPOSITORY_DIR
-python scripts/insert_imports.py $env:OCTOBOT_REPOSITORY_DIR/octobot/cli.py
-Copy-Item bin $env:OCTOBOT_REPOSITORY_DIR -recurse
-cd $env:OCTOBOT_REPOSITORY_DIR
-python ../scripts/fetch_nltk_data.py words $env:NLTK_DATA
-python -m PyInstaller bin/start.spec
+python packages/binary/scripts/python_file_lister.py bin/octobot_packages_files.txt
+python packages/binary/scripts/insert_imports.py octobot/cli.py
+python packages/binary/scripts/fetch_nltk_data.py words $env:NLTK_DATA
+python -m PyInstaller bin/start.spec --workpath installer
 Rename-Item dist/OctoBot.exe OctoBot_windows.exe
 Copy-Item dist/OctoBot_windows.exe OctoBot_windows.exe
 dist/OctoBot_windows.exe --version
