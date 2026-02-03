c18b91b393adbbebd7373c6cb271e555aaa1c8f7
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2026-02-03 19:24:57 +0100

    [Binary] add missing elements

--- packages/binary/build_scripts/windows.ps1
+++ packages/binary/build_scripts/windows.ps1
@@ -3,6 +3,6 @@ python packages/binary/scripts/python_file_lister.py bin/octobot_packages_files.
 python packages/binary/scripts/insert_imports.py octobot/cli.py
 python packages/binary/scripts/fetch_nltk_data.py words $env:NLTK_DATA
 python -m PyInstaller bin/start.spec --workpath installer
-Rename-Item dist/OctoBot.exe OctoBot_windows.exe
-Copy-Item dist/OctoBot_windows.exe OctoBot_windows.exe
-dist/OctoBot_windows.exe --version
+Move-Item dist\OctoBot.exe OctoBot_windows.exe
+.\OctoBot_windows.exe --version
+dir
