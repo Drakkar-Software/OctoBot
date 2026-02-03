8c1668ae97c8c9b6c12cf4e9986fb7ac43513ff6
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2026-02-03 15:17:23 +0100

    log dir

--- packages/binary/build_scripts/windows.ps1
+++ packages/binary/build_scripts/windows.ps1
@@ -3,5 +3,6 @@ python packages/binary/scripts/python_file_lister.py bin/octobot_packages_files.
 python packages/binary/scripts/insert_imports.py octobot/cli.py
 python packages/binary/scripts/fetch_nltk_data.py words $env:NLTK_DATA
 python -m PyInstaller bin/start.spec --workpath installer
-Move-Item dist/OctoBot.exe OctoBot_windows.exe
+Move-Item dist\OctoBot.exe OctoBot_windows.exe
+dir
 .\OctoBot_windows.exe --version
