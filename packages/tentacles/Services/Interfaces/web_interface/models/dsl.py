724e5ca7a9dea8e51a4f26679156c07f28293399
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2026-03-27 22:39:13 +0100

    [WebInterface] improve dsl docs

--- packages/tentacles/Services/Interfaces/web_interface/models/dsl.py
+++ packages/tentacles/Services/Interfaces/web_interface/models/dsl.py
@@ -14,22 +14,26 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 import octobot_commons.dsl_interpreter as dsl_interpreter
-import octobot_services.interfaces.util as interfaces_util
 
 import tentacles.Meta.DSL_operators.exchange_operators as dsl_operators
 
 
 def get_dsl_keywords_docs() -> list[dsl_interpreter.OperatorDocs]:
-    exchange_managers = interfaces_util.get_exchange_managers()
-    all_operators = list(dsl_interpreter.get_all_operators()) # copy list to avoid modifying the original (cached) list
-    if exchange_managers:
-        # include exchange related operators
-        all_operators += dsl_operators.create_ohlcv_operators(
-            exchange_managers[0], None, None
+    all_operators = (
+        dsl_interpreter.get_all_operators()
+        + dsl_operators.create_ohlcv_operators(
+            None, None, None
         )
-        all_operators += dsl_operators.create_portfolio_operators(
-            exchange_managers[0]
+        + dsl_operators.create_portfolio_operators(
+            None
         )
+        + dsl_operators.create_create_order_operators(
+            None,
+        )
+        + dsl_operators.create_cancel_order_operators(
+            None,
+        )
+    )
     return [
         operator.get_docs() 
         for operator in all_operators
