ae3822277abee04f2ccccc7efbacc225dfdea95c
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2026-03-27 15:48:37 +0100

    [Flow] add ohlcv TTL cache

--- packages/flow/octobot_flow/constants.py
+++ packages/flow/octobot_flow/constants.py
@@ -1,3 +1,5 @@
+import os
+
 import octobot_commons.os_util as os_util
 import octobot_commons.enums as commons_enums
 import octobot_commons.constants as commons_constants
@@ -8,4 +10,5 @@ SAVE_STATE_AFTER_EVERY_ACTION = os_util.parse_boolean_environment_var("SAVE_STAT
 DEFAULT_EXTERNAL_TRIGGER_ONLY_NO_ORDER_TIMEFRAME = commons_enums.TimeFrames.ONE_DAY
 
 # Caches settings
-TICKER_CACHE_TTL = 5 * commons_constants.MINUTE_TO_SECONDS
+TICKER_CACHE_TTL = int(os.getenv("TICKER_CACHE_TTL", "300"))
+OHLCV_CACHE_TTL = int(os.getenv("OHLCV_CACHE_TTL", "60"))
