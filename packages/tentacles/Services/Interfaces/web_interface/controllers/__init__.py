612c8a035639851915d80c77e09e55ccdaeee901
Herklos <herklos@drakkar.software>
2026-04-15 09:37:22 +0200

    [Node] Add NODE distribution support in web interface
    Signed-off-by: Herklos <herklos@drakkar.software>

--- packages/tentacles/Services/Interfaces/web_interface/controllers/__init__.py
+++ packages/tentacles/Services/Interfaces/web_interface/controllers/__init__.py
@@ -40,6 +40,7 @@ import tentacles.Services.Interfaces.web_interface.controllers.welcome
 import tentacles.Services.Interfaces.web_interface.controllers.robots
 import tentacles.Services.Interfaces.web_interface.controllers.distributions.market_making
 import tentacles.Services.Interfaces.web_interface.controllers.distributions.prediction_market
+import tentacles.Services.Interfaces.web_interface.controllers.distributions.node
 import tentacles.Services.Interfaces.web_interface.controllers.dsl
 
 
@@ -66,6 +67,8 @@ def register(blueprint, distribution: octobot.enums.OctoBotDistribution):
         tentacles.Services.Interfaces.web_interface.controllers.distributions.market_making.register(blueprint)
     elif distribution is octobot.enums.OctoBotDistribution.PREDICTION_MARKET:
         tentacles.Services.Interfaces.web_interface.controllers.distributions.prediction_market.register(blueprint)
+    elif distribution is octobot.enums.OctoBotDistribution.NODE:
+        tentacles.Services.Interfaces.web_interface.controllers.distributions.node.register(blueprint)
     # common routes
     tentacles.Services.Interfaces.web_interface.controllers.octobot_authentication.register(blueprint)
     tentacles.Services.Interfaces.web_interface.controllers.robots.register(blueprint)
